b5c147c9b785147f972d0794746369c1604b770d
ndom91 <yo@ndo.dev>
2021-07-12 23:18:05 +0200

    chore: fix eslint errors

--- scripts/version-pr.js
+++ scripts/version-pr.js
@@ -1,9 +1,9 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require('fs')
+const path = require('path')
 
 try {
-  const packageJSONPath = path.join(process.cwd(), "package.json")
-  const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, "utf8"))
+  const packageJSONPath = path.join(process.cwd(), 'package.json')
+  const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf8'))
 
   const sha8 = process.env.GITHUB_SHA.substr(0, 8)
   const prNumber = process.env.PR_NUMBER
@@ -12,6 +12,6 @@ try {
 
   fs.writeFileSync(packageJSONPath, JSON.stringify(packageJSON))
 } catch (error) {
-  console.error("Could not set PR version", error)
+  console.error('Could not set PR version', error)
   process.exit(1)
 }
