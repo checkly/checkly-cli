b348ef4b1453b0485da92d4fb35ba1fb0da05376
ndom91 <yo@ndo.dev>
2021-07-13 21:43:24 +0200

    fix: set commitlint reference to warning

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,11 +2,11 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 100],
-    'references-empty': [2, 'never'],
+    'references-empty': [1, 'never'],
   },
   parserPreset: {
     parserOpts: {
       issuePrefixes: ['[ch'],
     },
   },
-};
+}
