dd9cb36fd7d6c1e6b34d5822a19ec1f38a48bfd5
Nico Domino <yo@ndo.dev>
2021-07-29 16:07:36 +0200

    feat: basic project command plumbing (#10)

---
+++ src/commands/projects.js
@@ -0,0 +1,31 @@
+const { Command, flags } = require('@oclif/command')
+const projects = require('../modules/projects')
+const config = require('../services/config')
+
+const defaultOutput = config.get('output')
+
+class ProjectsCommand extends Command {
+  async run() {
+    const { args, flags } = this.parse(ProjectsCommand)
+
+    switch (args.action) {
+      case 'list':
+        return projects.list({ ...flags })
+      default:
+        return projects.list({ ...flags })
+    }
+  }
+}
+
+ProjectsCommand.description = 'Manage Checks'
+
+ProjectsCommand.flags = {
+  output: flags.string({
+    char: 'o',
+    description: 'output type',
+    default: defaultOutput,
+    options: ['text', 'json'],
+  }),
+}
+
+module.exports = ProjectsCommand
