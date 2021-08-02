37d8aaa1a267ba39b14597712dd2e9bafe509a9b
Nacho Anaya <nacho@checklyhq.com>
2021-08-02 10:14:52 -0300

    feat: improve print function and normalize outputs (#12)

--- src/services/api.js
+++ src/services/api.js
@@ -16,13 +16,13 @@ const api = axios.create({
 })
 
 api.interceptors.request.use(function (config) {
-  cli.action.start('Fetching..')
+  cli.action.start('Fetching')
   return config
 })
 
 api.interceptors.response.use(
   (res) => {
-    cli.action.stop('done')
+    cli.action.stop('done!')
     return res
   },
   (error) => {
