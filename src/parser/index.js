eab9c5e389d9b4f4d5dffdee473359e8f8e71e1d
Ignacio Anaya <nacho@checklyhq.com>
2021-08-03 16:57:45 -0300

    feat: add diff module [PoC]

--- src/parser/index.js
+++ src/parser/index.js
@@ -2,3 +2,5 @@ const { parseChecklyDirectory } = require('./file-parser')
 const { parseChecksTree } = require('./check-parser')
 
 module.exports = () => parseChecksTree(parseChecklyDirectory())
+
+console.log(JSON.stringify(parseChecksTree(parseChecklyDirectory()), null, 2))
