2404a0f4e3e9027c5e212d1bd36f6c70cd68950d
Ignacio Anaya <nacho@checklyhq.com>
2021-08-03 16:58:59 -0300

    chore: remove console output from parser

--- src/parser/index.js
+++ src/parser/index.js
@@ -2,5 +2,3 @@ const { parseChecklyDirectory } = require('./file-parser')
 const { parseChecksTree } = require('./check-parser')
 
 module.exports = () => parseChecksTree(parseChecklyDirectory())
-
-console.log(JSON.stringify(parseChecksTree(parseChecklyDirectory()), null, 2))
