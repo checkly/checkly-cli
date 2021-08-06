820506768e5a09652d2bc991527fe53e2a546fde
Nacho Anaya <nacho@checklyhq.com>
2021-08-06 09:04:39 -0300

    feat: add commonjs bundle [PoC] (#16)
    * feat: add commonjs bundle [PoC]
    
    * chore: remove rollup source map plugin
    
    * chore: resolve conflicts in parser
    
    * chore: remove iife from bundler

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,4 +1,4 @@
 const { parseChecklyDirectory } = require('./file-parser')
 const { parseChecksTree } = require('./check-parser')
 
-module.exports = () => parseChecksTree(parseChecklyDirectory())
+module.exports = async () => await parseChecksTree(parseChecklyDirectory())
