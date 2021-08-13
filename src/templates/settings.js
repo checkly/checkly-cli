31f446186d73c62118a3ea8f03096fe98e904a21
Nacho Anaya <nacho@checklyhq.com>
2021-08-13 11:26:39 -0300

    refactor: add promtps and tweak init/login flows (#19)
    * refactor: remove env var from login and add inquirer questions
    
    * refactor: add prompts to init and advance/basic templates
    
    * chore: add account data todo
    
    * fix: avoid wrong group level

---
+++ src/templates/settings.js
@@ -0,0 +1,17 @@
+module.exports = ({ accountId, accountName, projectId, projectName }) => {
+  return `account:
+  - id: ${accountId}
+    name: ${accountName}
+project:
+  - id: ${projectId}
+    name: ${projectName}
+settings:
+  - locations: ['us-east-1', 'eu-central-1']
+    interval: 5min
+    alerts:
+      - type: email
+        sendOn:
+          - recover
+          - degrade
+          - fail`
+}
