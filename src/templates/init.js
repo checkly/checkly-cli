e57c305d6b915d3d371c8751b1659b535c82740a
ndom91 <yo@ndo.dev>
2021-08-20 13:49:32 +0200

    fix: run cmd error handling

--- src/templates/init.js
+++ src/templates/init.js
@@ -11,7 +11,7 @@ project:
   id: ${projectId}
   name: ${projectName}
 checkDefaults:
-  locations: ['us-east-1', 'eu-central-1']
+  locations: ["us-east-1", "eu-central-1"]
   interval: 5min
   alerts:
     - type: email
@@ -30,7 +30,7 @@ url: https://jsonplaceholder.typicode.com/users
 
 const defaultCheckTemplate = () => {
   return `checkType: BROWSER
-name: 'example'
+name: "example"
 frequency: 10
 activated: true
 muted: false
