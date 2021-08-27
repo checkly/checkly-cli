300aad9619e5f6130b6ea21dc309f8234090d846
ndom91 <yo@ndo.dev>
2021-08-27 14:56:34 +0200

    feat: rewrite run cmd

---
+++ sdk/helper.js
@@ -0,0 +1,19 @@
+const { join } = require('path')
+const { statSync, readdirSync } = require('fs')
+
+const getLocalFiles = (path, fileContents = []) => {
+  const fileNames = readdirSync(path)
+
+  fileNames.forEach((file) => {
+    if (statSync(path + '/' + file).isDirectory()) {
+      fileContents = getLocalFiles(path + '/' + file, fileContents)
+    } else {
+      fileContents.push(join(path, '/', file))
+    }
+  })
+  return fileContents
+}
+
+module.exports = {
+  getLocalFiles,
+}
