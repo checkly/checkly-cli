6f08e2eb3b412ec4e151dda8dc263525dc26bbcf
Nico Domino <yo@ndo.dev>
2021-09-20 17:41:02 +0200

    feat: whitelist packages via custom rollup plugin (#41)

--- src/parser/bundler.js
+++ src/parser/bundler.js
@@ -1,20 +1,24 @@
 const path = require('path')
 const rollup = require('rollup')
 const commonjs = require('@rollup/plugin-commonjs')
+const checklyWhitelist = require('../services/rollup-plugin-checkly-whitelist')
 
 const OUTPUT_DIRECTORY = '../../.checkly/output'
 
-async function bundle(entryFile, writeBundle = false) {
+async function bundle(check, writeBundle = false) {
   const inputOptions = {
-    input: path.join(process.cwd(), entryFile),
-    plugins: [commonjs({ sourceMap: true })],
+    input: path.join(process.cwd(), check.path),
+    plugins: [
+      commonjs({ sourceMap: true }),
+      checklyWhitelist({ runtime: check.runtime || '2021.06' }),
+    ],
   }
 
   const outputOptions = {
     sourcemap: true,
     exports: 'auto',
     dir: path.join(__dirname, OUTPUT_DIRECTORY),
-    format: 'cjs',
+    format: 'es',
   }
 
   const bundle = await rollup.rollup(inputOptions)
---
+++ src/services/rollup-plugin-checkly-whitelist.js
@@ -0,0 +1,36 @@
+const https = require('https')
+const consola = require('consola')
+const chalk = require('chalk')
+
+module.exports = function plugin(options = {}) {
+  return {
+    name: 'checkly-whitelist',
+    async generateBundle(bundleOptions, bundle, isWrite) {
+      https.get('https://api.checklyhq.com/v1/runtimes', (res) => {
+        let data = ''
+
+        res.on('data', (chunk) => {
+          data += chunk
+        })
+
+        res.on('end', () => {
+          const runtimes = JSON.parse(data)
+          const runtime = runtimes.find(
+            (runtime) => runtime.name === options.runtime
+          )
+          const latestRuntimeDeps = Object.keys(runtime.dependencies)
+
+          Object.values(bundle).forEach((script) => {
+            script.imports.forEach((i) => {
+              if (!latestRuntimeDeps.includes(i)) {
+                console.log('\n')
+                consola.error(`Package ${chalk.blue.bold(i)} not allowed!`)
+                this.error('Invalid import in required Checkly check script.')
+              }
+            })
+          })
+        })
+      })
+    },
+  }
+}
