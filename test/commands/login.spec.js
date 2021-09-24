d16d2c1832d3102524482b9a7dadefe9e872d6bb
ndom91 <yo@ndo.dev>
2021-09-24 22:09:02 +0200

    test: stubbing inquirer

--- test/commands/login.spec.js
+++ test/commands/login.spec.js
@@ -2,17 +2,20 @@
 const os = require('os')
 const inquirer = require('inquirer')
 const { test, expect } = require('@oclif/test')
-const { defaultConfig } = require('../fixtures/config')
+// const { defaultConfig } = require('../fixtures/config')
 
 describe('login', function () {
   test
     // .nock('http://localhost:3000', (api) => api.get('/v1/checks').reply(401))
     // .loadConfig(defaultConfig)
-    .stub(inquirer, 'prompt', () => 'Do you want to replace key?')
-    .stdout()
+    // .stub(inquirer, 'prompt', () => Promise.resolve({ setNewKey: true }))
+    // .stub(inquirer, 'prompt', () => Promise.resolve({ setNewKey: 'n\n' }))
     .stdin(`n${os.EOL}`)
+    .stdout()
     .command(['login', '--apiKey', '123'])
-    .it('login answer', (ctx) => {
-      expect(inquirer.prompt).to.equal(true)
-    })
+    .exit(1)
+    .it('login answer')
+  // .it('login answer', (ctx) => {
+  //   expect(inquirer.prompt).to.equal(true)
+  // })
 })
