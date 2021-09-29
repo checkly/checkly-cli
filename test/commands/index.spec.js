9c05da00db0a0d6842f9c335f1925e78af985f92
Nico Domino <yo@ndo.dev>
2021-09-29 10:45:26 +0200

    feat: mocha tests (#63)

---
+++ test/commands/index.spec.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+const { test, expect } = require('@oclif/test')
+const { defaultConfig } = require('../fixtures/config')
+
+describe('version', () => {
+  test
+    .loadConfig(defaultConfig)
+    .stdout()
+    .command(['help'])
+    .it('prints version number', (ctx) =>
+      expect(ctx.stdout).to.contain(`
+USAGE
+  $ checkly [COMMAND]
+
+COMMANDS
+  add       Add a new group or check file
+  checks    Manage Checks
+  conf      manage configuration
+  deploy    Deploy and sync your ./checkly directory
+  groups    Manage Groups
+  help      display help for checkly
+  init      Initialise a new Checkly Project
+  login     Login with a Checkly API Key [WIP]
+  logout    Logout and clear local conf
+  projects  Manage Checks
+  run       Run and test your checks on Checkly
+  status    Status dashboard`)
+    )
+})
---
+++ test/commands/login.spec.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+const os = require('os')
+const { test } = require('@oclif/test')
+
+describe('login', function () {
+  test
+    .nock('http://localhost:3000', (api) =>
+      api.get('/v1/account').reply(200, {
+        accountId: 'abc123mockaccountId',
+        name: 'NockAccount',
+      })
+    )
+    .stdin(`y${os.EOL}`, 500) // answer inquirer prompt "y" with 500ms delay
+    .stdout({ print: true })
+    .command(['login', '--apiKey', '123'])
+    .it('login answer')
+})
