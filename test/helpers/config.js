0ea16a3d2e74586405402202cec975173ee1e912
Ignacio Anaya <nacho@checklyhq.com>
2021-09-30 16:03:17 -0300

    chore: clean up test config

--- test/helpers/config.js
+++ test/helpers/config.js
@@ -5,26 +5,7 @@ const testConfig = () => {
     dataTest: new Conf({
       configName: 'data-test',
       defaults: {
-        env: 'test',
-        version: '0.0.1',
         output: 'json',
-        production: {
-          apiUrl: 'https://api.checklyhq.com',
-          apiVersion: 'v1',
-        },
-        development: {
-          apiUrl: 'http://localhost:3000',
-          apiVersion: 'v1',
-        },
-        test: {
-          apiUrl: 'http://localhost:3000',
-          apiVersion: 'v1',
-        },
-        staging: {
-          apiUrl: 'https://api.checklyhq.com',
-          apiVersion: 'v1',
-        },
-        isInitialized: 'true',
         accountName: 'Test Account',
         accountId: 'abc123',
       },
