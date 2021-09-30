c12a7abb760dc64f0572b53587be73a77cd1df41
Ignacio Anaya <nacho@checklyhq.com>
2021-09-30 11:08:27 -0300

    fix: update api module name in login and whoami

--- src/commands/whoami.js
+++ src/commands/whoami.js
@@ -1,12 +1,12 @@
 const { Command } = require('@oclif/command')
-const { account } = require('./../services/api')
+const { accounts } = require('./../services/api')
 const { output } = require('./../services/flags')
 const { print } = require('./../services/utils')
 
 class WhoamiCommand extends Command {
   async run() {
     const { flags } = this.parse(WhoamiCommand)
-    const { data } = await account.findMe()
+    const { data } = await accounts.findMe()
     print(data, flags)
   }
 }
