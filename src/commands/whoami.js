b9732fdd933166904f061b973742b285fa803e5f
Ignacio Anaya <nacho@checklyhq.com>
2021-11-04 15:36:43 -0300

    chore: add env variable warning to whoami

--- src/commands/whoami.js
+++ src/commands/whoami.js
@@ -1,4 +1,6 @@
+const consola = require('consola')
 const { Command } = require('@oclif/command')
+
 const config = require('./../services/config')
 const { output } = require('./../services/flags')
 const { print } = require('./../services/utils')
@@ -6,10 +8,21 @@ const { print } = require('./../services/utils')
 class WhoamiCommand extends Command {
   async run() {
     const { flags } = this.parse(WhoamiCommand)
+
+    let accountName = config.data.get('accountName')
+    const accountId = config.getAccountId()
+
+    if (process.env.CHECKLY_ACCOUNT_ID) {
+      accountName = '-'
+      consola.warn(
+        "Account ID it's being read from CHECKLY_ACCOUNT_ID env variable"
+      )
+    }
+
     print(
       {
-        accountId: config.data.get('accountId'),
-        accountName: config.data.get('accountName'),
+        accountId,
+        accountName,
       },
       flags
     )
