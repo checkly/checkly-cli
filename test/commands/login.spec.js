e9ba89db6a6842cbf1322194d75d63c4df38b376
ndom91 <yo@ndo.dev>
2021-12-07 19:57:48 +0100

    feat: begin E2E login test

--- test/commands/login.spec.js
+++ test/commands/login.spec.js
@@ -1,8 +1,13 @@
 const testConfig = require('../helpers/config')
+const ReadlineStub = require('../helpers/readline')
 const config = require('./../../src/services/config')
 const { test, expect } = require('@oclif/test')
 
 describe('login [cmd]', () => {
+  beforeEach(function () {
+    this.rl = new ReadlineStub()
+  })
+
   before(() => {
     testConfig()
   })
@@ -15,5 +20,18 @@ describe('login [cmd]', () => {
       expect(ctx.stdout).to.contain('Welcome to @checkly/cli')
       expect(config.auth.get('apiKey')).to.equal('newKey')
       expect(config.getAccountId()).to.equal('newId')
+    })
+
+  // Test real login process
+  test
+    .stdout()
+    .command(['login'])
+    .exit(0)
+    .it('starts localhost:4242', (ctx) => {
+      // Ensure server has started and is listening on port 4242
+      console.log('rl', this.rl.output)
+      console.log('rl1', this.rl.output.__raw__)
+      console.log('ctx', ctx.stdout)
+      expect(ctx.stdout).to.contain('Existing session')
     })
 })
---
+++ test/helpers/readline.js
@@ -0,0 +1,36 @@
+const { EventEmitter } = require('events')
+const sinon = require('sinon')
+const util = require('util')
+
+const stub = {}
+
+Object.assign(stub, {
+  write: sinon.stub().returns(stub),
+  moveCursor: sinon.stub().returns(stub),
+  setPrompt: sinon.stub().returns(stub),
+  close: sinon.stub().returns(stub),
+  pause: sinon.stub().returns(stub),
+  resume: sinon.stub().returns(stub),
+  _getCursorPos: sinon.stub().returns({ cols: 0, rows: 0 }),
+  output: {
+    end: sinon.stub(),
+    mute: sinon.stub(),
+    unmute: sinon.stub(),
+    __raw__: '',
+    write(str) {
+      this.__raw__ += str
+    },
+  },
+})
+
+const ReadlineStub = function () {
+  this.line = ''
+  this.input = new EventEmitter()
+  // eslint-disable-next-line
+  EventEmitter.apply(this, arguments)
+}
+
+util.inherits(ReadlineStub, EventEmitter)
+Object.assign(ReadlineStub.prototype, stub)
+
+module.exports = ReadlineStub
