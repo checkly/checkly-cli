0733e751818db29e439087a6c91342aaa422e37d
Umut Uzgur <umutuzgur@gmail.com>
2021-12-20 11:41:57 +0100

    fix: catch dependencies that we are not supported (#134)

--- src/services/rollup-plugin-checkly-whitelist.js
+++ src/services/rollup-plugin-checkly-whitelist.js
@@ -1,34 +1,26 @@
-const https = require('https')
-const consola = require('consola')
+const axios = require('axios')
 const chalk = require('chalk')
 
 module.exports = function plugin (options = {}) {
   return {
     name: 'checkly-whitelist',
     async generateBundle (bundleOptions, bundle) {
-      https.get('https://api.checklyhq.com/v1/runtimes', (res) => {
-        let data = ''
+      const { data } = await axios.get('https://api.checklyhq.com/v1/runtimes')
 
-        res.on('data', (chunk) => {
-          data += chunk
-        })
+      const runtime = data.find((runtime) => runtime.name === options.runtime)
+      if (!runtime) {
+        data.find((runtime) => runtime.default)
+      }
 
-        res.on('end', () => {
-          const runtimes = JSON.parse(data)
-          const runtime = runtimes.find(
-            (runtime) => runtime.name === options.runtime
-          )
-          const latestRuntimeDeps = Object.keys(runtime.dependencies)
+      const latestRuntimeDeps = Object.keys(runtime.dependencies)
 
-          Object.values(bundle).forEach((script) => {
-            script.imports.forEach((i) => {
-              if (!latestRuntimeDeps.includes(i)) {
-                console.log('\n')
-                consola.error(`Package ${chalk.blue.bold(i)} not allowed!`)
-                this.error('Invalid import in required Checkly check script.')
-              }
-            })
-          })
+      Object.values(bundle).forEach((script) => {
+        script.imports.forEach((i) => {
+          if (!latestRuntimeDeps.includes(i)) {
+            this.error(
+              `Invalid import of ${chalk.blue.bold(i)} package in check script.`
+            )
+          }
         })
       })
     }
