6bdcff5450045cf7b9c3506d8a19db75951e0191
Ignacio Anaya <nacho@checklyhq.com>
2022-01-02 23:51:06 -0300

    fix: remove extra char in projects URL

--- sdk/api/modules/projects.js
+++ sdk/api/modules/projects.js
@@ -2,20 +2,20 @@ const PATH = 'projects'
 
 const projects = ({ api, apiVersion = 'next' }) => {
   function getAll () {
-    return api.get(`/${apiVersion}}/${PATH}`)
+    return api.get(`/${apiVersion}/${PATH}`)
   }
 
   function create (project) {
-    return api.post(`/${apiVersion}}/${PATH}`, project)
+    return api.post(`/${apiVersion}/${PATH}`, project)
   }
 
   function deleteProject (id) {
-    return api.delete(`/${apiVersion}}/${PATH}/${id}?newSync=true`)
+    return api.delete(`/${apiVersion}/${PATH}/${id}?newSync=true`)
   }
 
-  function deploy (resources, { dryRun = false } = {}) {
+  function deploy (resources, { dryRun = false, newSync = true } = {}) {
     return api.post(
-      `/${apiVersion}/${PATH}/deploy?dryRun=${dryRun}&newSync=true`,
+      `/${apiVersion}/${PATH}/deploy?dryRun=${dryRun}&newSync=${newSync}`,
       resources
     )
   }
