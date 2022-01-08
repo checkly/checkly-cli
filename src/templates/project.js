53cfd0316835815ac7d9fab3d335368b6242e77b
Ignacio Anaya <nacho@checklyhq.com>
2022-01-08 00:06:19 -0300

    refactor: simplify project template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,27 +1,11 @@
-module.exports = ({ projectId, projectName, locations = ['us-east-1', 'eu-central-1'] } = {}) => {
+module.exports = ({ projectId, projectName, runtimeId = '2021.10', locations = ['us-east-1', 'eu-central-1'] } = {}) => {
   return `projectId: ${projectId}
 projectName: ${projectName}
 
 # Default check settings will be used when you don't specify  them at check or group level
 defaultCheckSettings:
-  frequency: 10
-  runtimeId: "2021.10"
+  runtimeId: ${runtimeId}
   locations:
     - ${locations.join('\n  - ')}
-  activated: true
-  muted: false
-  doubleCheck: true
-  alertSettings:
-    escalationType: "RUN_BASED"
-    runBasedEscalation:
-      failedRunThreshold: 1
-    timeBasedEscalation:
-      minutesFailingThreshold: 5
-    reminders:
-      amount: 0
-      interval: 5
-    sslCertificates:
-      enabled: false
-      alertThreshold: 30
 `
 }
