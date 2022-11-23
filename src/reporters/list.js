4802ee7940ffe58151ed6a3f8aa9945ad320d823
Chris Lample <chris@checklyhq.com>
2022-11-23 17:51:49 +0100

    feat: improve the result reporting

--- src/reporters/list.js
+++ src/reporters/list.js
@@ -8,7 +8,7 @@ class ListReporter {
   onBegin (checks) {
     this.checks = checks
     this.results = []
-
+    this.inProgressChecks = {}
     this._printSummary()
   }
 
@@ -16,29 +16,42 @@ class ListReporter {
 
   }
 
-  onCheckBegin () {
-
-  }
-
-  onCheckEnd (checkResult) {
-    this.results.push(checkResult)
+  onCheckBegin (check) {
+    this.inProgressChecks[check.logicalId] = check
+    this._clearSummary()
     this._printSummary()
   }
 
-  _numFailedChecks () {
-    return this.results.filter(({ hasFailures }) => hasFailures).length
+  onCheckEnd (checkResult) {
+    delete this.inProgressChecks[checkResult.logicalId]
+    this.results.push(checkResult)
+
+    // We want the SUCCEEDED/FAILED status to be above the summary, so we clear the summary first
+    this._clearSummary()
+    if (checkResult.hasFailures) {
+      console.log(chalk.red(`${checkResult.name} - FAILED`))
+    } else {
+      console.log(chalk.green(`${checkResult.name} - SUCCEEDED`))
+    }
+
+    this._printSummary()
   }
 
-  _numSuccessfulChecks () {
-    return this.results.filter(({ hasFailures }) => !hasFailures).length
+  // Clear the summary which was printed by _printSummary from stdout
+  _clearSummary () {
+    console.log(this._clearString)
   }
 
   _printSummary () {
+    const inProgressCheckSummaries = Object.values(this.inProgressChecks)
+      .map(({ name }) => chalk.yellow(`${name} - IN PROGRESS`))
+
+    // TODO: We will probably need to skip the interactive summary in CI environments
     const numFailedChecks = this.results.filter(({ hasFailures }) => hasFailures).length
     const numSuccessfulChecks = this.results.filter(({ hasFailures }) => !hasFailures).length
     const numPendingChecks = this.checks.length - numFailedChecks - numSuccessfulChecks
     const totalChecks = this.checks.length
-    const checks = `${
+    const checkCountOverview = `${
         chalk.bold('Checks:    ') +
         (numFailedChecks > 0 ? `${chalk.bold.red(`${numFailedChecks} failed`)}, ` : '') +
         (numPendingChecks > 0 ? `${chalk.bold.magenta(`${numPendingChecks} pending`)}, ` : '') +
@@ -49,8 +62,7 @@ class ListReporter {
     const length = Math.floor(statusBarWidth * (totalChecks - numPendingChecks) / totalChecks)
     const statusBar = `${chalk.green('█').repeat(length)}${chalk.white('█').repeat(statusBarWidth - length)}`
 
-    const summary = [checks, statusBar].join('\n')
-    console.log(this._clearString)
+    const summary = [...inProgressCheckSummaries, checkCountOverview, statusBar].join('\n')
     console.log(summary)
     // Ansi escape code for erasing the line and moving the cursor up
     this._clearString = '\u001B[2K\u001B[1A'.repeat(summary.split('\n').length + 1)
--- src/reporters/test.js
+++ src/reporters/test.js
@@ -6,29 +6,34 @@ function sleep (ms) {
 
 async function main () {
   const listReporter = new ListReporter()
-  listReporter.onBegin([
-    { name: 'Check 1' },
-    { name: 'Check 2' },
-    { name: 'Check 3' },
-    { name: 'Check 4' },
-    { name: 'Check 5' },
-  ])
+  const check1 = { name: 'Check 1', logicalId: 'check-1' }
+  const check2 = { name: 'Check 2', logicalId: 'check-2' }
+  const check3 = { name: 'Check 3', logicalId: 'check-3' }
+  const check4 = { name: 'Check 4', logicalId: 'check-4' }
+  const check5 = { name: 'Check 5', logicalId: 'check-5' }
+  listReporter.onBegin([check1, check2, check3, check4, check5])
 
+  listReporter.onCheckBegin(check1)
   await sleep(2000)
 
-  listReporter.onCheckEnd({ name: 'Check 1', hasFailures: true })
+  listReporter.onCheckEnd({ ...check1, hasFailures: true })
+
+  // Imagine checks 2 and 3 run concurrently...
+  listReporter.onCheckBegin(check3)
+  listReporter.onCheckBegin(check2)
+  await sleep(1000)
+  listReporter.onCheckEnd({ ...check2, hasFailures: false })
 
   await sleep(1000)
-  listReporter.onCheckEnd({ name: 'Check 2', hasFailures: false })
+  listReporter.onCheckEnd({ ...check3, hasFailure: false })
 
+  listReporter.onCheckBegin(check4)
   await sleep(1000)
-  listReporter.onCheckEnd({ name: 'Check 3', hasFailures: false })
+  listReporter.onCheckEnd({ ...check4, hasFailures: true })
 
+  listReporter.onCheckBegin(check5)
   await sleep(1000)
-  listReporter.onCheckEnd({ name: 'Check 4', hasFailures: false })
-
-  await sleep(1000)
-  listReporter.onCheckEnd({ name: 'Check 5', hasFailures: true })
+  listReporter.onCheckEnd({ ...check5, hasFailures: true })
 }
 
 main()
