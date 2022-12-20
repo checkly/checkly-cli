53d97e7c62d9b38feab40609057905d93c97c2b1
Umut Uzgur <umutuzgur@gmail.com>
2022-12-20 12:20:03 +0100

    chore: rename the var according to the PR review [sc-0]

--- src/constructs/sms-alert-channel.ts
+++ src/constructs/sms-alert-channel.ts
@@ -1,14 +1,14 @@
 import { AlertChannel, AlertChannelProps } from './alert-channel'
 
 export interface SmsAlertChannelProps extends AlertChannelProps {
-    number: string
+    phoneNumber: string
 }
 
 export class SmsAlertChannel extends AlertChannel {
-  number: string
+  phoneNumber: string
   constructor (logicalId: string, props: SmsAlertChannelProps) {
     super(logicalId, props)
-    this.number = props.number
+    this.phoneNumber = props.phoneNumber
     this.register(AlertChannel.__checklyType, logicalId, this.synthesize())
   }
 
@@ -17,7 +17,7 @@ export class SmsAlertChannel extends AlertChannel {
       ...super.synthesize(),
       type: 'SMS',
       config: {
-        number: this.number,
+        number: this.phoneNumber,
       },
     }
   }
