3110ef45c9bf5d918b6300e9a5127923288a4291
Umut Uzgur <umutuzgur@gmail.com>
2022-12-20 22:17:37 +0100

    feat: add the deploy command [sc-0]

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,4 +1,7 @@
+import * as api from '../rest/api'
+import { prompt } from 'inquirer'
 import { Command, Flags } from '@oclif/core'
+import { parseProject } from '../services/project-parser'
 
 export default class Deploy extends Command {
   static description = 'Deploy your changes'
@@ -14,6 +17,24 @@ export default class Deploy extends Command {
   async run (): Promise<void> {
     const { flags } = await this.parse(Deploy)
 
-    this.log(`Running the deployment (preview=${flags.preview})`)
+    const project = await parseProject(process.cwd())
+
+    const { confirm } = await prompt([{
+      name: 'confirm',
+      type: 'confirm',
+      message: 'You are about to deploy your project. Do you want to continue?',
+    }])
+    if (!confirm) {
+      return
+    }
+
+    try {
+      await api.projects.deploy(project.synthesize())
+    } catch (err: any) {
+      if (err?.response?.status === 400) {
+        console.error(`Failed to deploy the project due to a missing field. ${err.response.data.message}`)
+      }
+      throw err
+    }
   }
 }
