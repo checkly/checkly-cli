2c64707c0fb41e07a7c6e20f6e73075f8ab927af
Chris Lample <chrislample@protonmail.com>
2022-12-21 17:21:45 +0100

    feat: add test command for running checks (#305)

---
+++ src/commands/test.ts
@@ -0,0 +1,31 @@
+import { Command, Flags } from '@oclif/core'
+import ListReporter from '../reporters/list'
+import { parseProject } from '../services/project-parser'
+import { runChecks } from '../services/check-runner'
+
+export default class Test extends Command {
+  static description = 'Test checks on Checkly'
+  static flags = {
+    location: Flags.string({
+      char: 'l',
+      description: 'The location to run the checks on',
+      default: 'eu-central-1',
+    }),
+  }
+  // TODO: Add flags for running specific checks
+
+  async run (): Promise<void> {
+    const { flags } = await this.parse(Test)
+    const { location } = flags
+
+    const project = await parseProject(process.cwd())
+    const { checks: checksMap } = project.data
+    const checks = Object.entries(checksMap).map(([key, check]) => {
+      check.logicalId = key
+      return check
+    })
+    const reporter = new ListReporter(checks)
+    await runChecks(checks, location, reporter)
+    // TODO - non-zero status code if checks fail
+  }
+}
