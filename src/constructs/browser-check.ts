b3d410a0d5b166568faa38dc7863c5fe2a3330a3
Umut Uzgur <umutuzgur@gmail.com>
2022-12-21 14:50:46 +0100

    chore: add sample project [sc-0]

--- src/constructs/browser-check.ts
+++ src/constructs/browser-check.ts
@@ -1,28 +1,61 @@
+import * as fs from 'fs/promises'
 import { Check, CheckProps } from './check'
-
-export interface BrowserCheckProps extends CheckProps {
-  script: string
-  scriptPath?: string
-  dependencies?: Array<string>
-}
+import { parseDependencies } from '../services/check-dependency-parser'
 
 export interface CheckDependency {
   path: string
   content: string
 }
 
+export interface Bundle {
+  script: string
+  scriptPath: string
+  dependencies: Array<CheckDependency>
+}
+
+export interface BrowserCheckProps extends CheckProps {
+  code: string|Bundle
+}
+
 export class BrowserCheck extends Check {
   script: string
   scriptPath?: string
-  dependencies?: Array<string>
+  dependencies?: Array<CheckDependency>
 
   constructor (logicalId: string, props: BrowserCheckProps) {
     super(logicalId, props)
-    this.script = props.script
-    this.scriptPath = props.scriptPath
-    this.dependencies = props.dependencies
+    if (props.code instanceof String) {
+      const script = props.code as string
+      this.script = script
+    } else {
+      const bundle = props.code as Bundle
+      this.script = bundle.script
+      this.scriptPath = bundle.scriptPath
+      this.dependencies = bundle.dependencies
+    }
     this.register(Check.__checklyType, this.logicalId, this.synthesize())
     this.addSubscriptions()
+  }
+
+  static async bundle (entry: string): Promise<Bundle> {
+    // TODO: We need pass the runtimeId somehow
+    const parsed = await parseDependencies(entry)
+    // Maybe we can get the parsed deps with the content immediately
+    const content = await fs.readFile(entry, { encoding: 'utf8' })
+
+    const deps: CheckDependency[] = []
+    for (const dep of parsed) {
+      const content = await fs.readFile(entry, { encoding: 'utf8' })
+      deps.push({
+        path: dep,
+        content,
+      })
+    }
+    return {
+      script: content,
+      scriptPath: entry,
+      dependencies: deps,
+    }
   }
 
   synthesize () {
