f28a2422e8c3ad52142a37e352d4d396991a0989
tnolet <tim@checklyhq.com>
2022-12-30 14:34:05 +0100

    feat: adds filtering by filename and check name

---
+++ package/src/commands/helpers/test-filters.ts
@@ -0,0 +1,11 @@
+export function filterByCheckNamePattern (checkNamePattern = '', checkName: string) {
+  const re = new RegExp(checkNamePattern)
+  return re.test(checkName)
+}
+
+export function filterByFileNamePattern (filePatterns: Array<string> = [], path: string) {
+  return !!filePatterns.find(filePattern => {
+    const re = new RegExp(filePattern)
+    return re.test(path)
+  })
+}
