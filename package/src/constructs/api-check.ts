8b57b8e78f382dee58a89a38e3e430c84322898a
Umut Uzgur <umutuzgur@gmail.com>
2023-01-02 17:07:48 +0100

    feat: add further api check props [sc-0] (#353)

--- package/src/constructs/api-check.ts
+++ package/src/constructs/api-check.ts
@@ -1,21 +1,41 @@
 import { Check, CheckProps } from './check'
 
+import KeyValuePair from './key-value-pair'
+export type QueryParamProps = KeyValuePair
+export type HeaderProps = KeyValuePair
 export interface Assertion {
-    source: string,
-    property: string,
-    comparison: string,
-    target: string
-    regex: string
+  source: string,
+  property: string,
+  comparison: string,
+  target: string
+  regex: string
+}
+
+enum BodyType {
+  JSON,
+  FORM,
+  RAW,
+  GRAPHQL,
+  NONE
+}
+
+interface BasicAuth {
+  username: string
+  password: string
 }
 
 export interface Request {
-    url: string,
-    method: string,
-    followRedirects: boolean,
-    skipSsl: boolean,
-    assertions: Array<Assertion>
+  url: string,
+  method: string,
+  followRedirects: boolean,
+  skipSsl: boolean,
+  assertions: Array<Assertion>
+  body?: string
+  bodyType?: BodyType
+  headers?: Array<HeaderProps>
+  queryParams?: Array<QueryParamProps>
+  basicAuth?: BasicAuth
 }
-
 export interface ApiCheckProps extends CheckProps {
   request: Request
 }
--- package/src/constructs/environment-variable.ts
+++ package/src/constructs/environment-variable.ts
@@ -1,8 +1,5 @@
-export interface EnvironmentVariableProps {
-  key: string
-  value: string
-  locked: boolean
-}
+import KeyValuePair from './key-value-pair'
+export type EnvironmentVariableProps = KeyValuePair
 
 export class EnvironmentVariable {
   key: string
@@ -11,6 +8,6 @@ export class EnvironmentVariable {
   constructor (props: EnvironmentVariableProps) {
     this.key = props.key
     this.value = props.value
-    this.locked = props.locked
+    this.locked = props.locked || false
   }
 }
