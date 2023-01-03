e1931d1096528e66912145fb2a80b3229d344dda
Umut Uzgur <umutuzgur@gmail.com>
2023-01-03 10:39:18 +0100

    chore: move fixtures to a folder, add error and cleanup directory logic [sc-0]

--- package/e2e/__tests__/check-parse-error.spec.ts
+++ package/e2e/__tests__/check-parse-error.spec.ts
@@ -1,10 +1,11 @@
+import * as path from 'path'
 import { runChecklyCli } from '../run-checkly'
 
 describe('check parse error', () => {
   it('"checkly test" should return a clear error when there are check dependency errors', () => {
     const result = runChecklyCli({
       args: ['test'],
-      directory: 'check-parse-error'
+      directory: path.join(__dirname, 'fixtures/check-parse-error'),
     })
     expect(result.stderr).toMatchSnapshot()
   })
--- package/src/constructs/construct.ts
+++ package/src/constructs/construct.ts
@@ -7,6 +7,9 @@ export class Construct {
   }
 
   register (type: string, logicalId: string, resource: any) {
+    if (!Session.project) {
+      throw new Error('A project is not registered to the session')
+    }
     Session.project.addResource(type, logicalId, resource)
   }
 }
