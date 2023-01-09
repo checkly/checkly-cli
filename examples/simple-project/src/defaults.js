a611222b2917a4414f75be49b5a25d2e623283b6
tnolet <tim@checklyhq.com>
2023-01-09 16:58:05 +0100

    chore: adds readme for example and GH Actions workflow

--- examples/simple-project/src/defaults.js
+++ examples/simple-project/src/defaults.js
@@ -1,5 +1,5 @@
 const defaults = {
-  pageUrl: process.env.ENVIRONMENT_URL || 'https://checklyhq.com',
+  pageUrl: process.env.ENVIRONMENT_URL || 'https://checklyhq.com', // the pageUrl can be replaced by urls from a preview / staging environment.
   playwright: {
     viewportSize: { width: 1920, height: 1080 },
   },
