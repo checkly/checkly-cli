02fd7e53e1484c166cc894bd6296672e742a8613
Chris Lample <chris@checklyhq.com>
2023-01-11 14:52:55 +0100

    chore: remove unused API path config

--- package/src/rest/api.ts
+++ package/src/rest/api.ts
@@ -12,33 +12,28 @@ export function getDefaults () {
   const environments = {
     production: {
       apiUrl: 'https://api.checklyhq.com',
-      apiVersion: 'v1',
     },
 
     development: {
       apiUrl: 'http://localhost:3000',
-      apiVersion: 'v1',
     },
 
     test: {
       apiUrl: 'https://api-test.checklyhq.com',
-      apiVersion: 'v1',
     },
 
     staging: {
       apiUrl: 'https://api-test.checklyhq.com',
-      apiVersion: 'v1',
     },
   }
 
   const env = config.getEnv()
   const apiKey = config.getApiKey()
   const accountId = config.getAccountId()
-  const basePath = environments[env].apiVersion
   const baseURL = environments[env].apiUrl
   const Authorization = `Bearer ${apiKey}`
 
-  return { baseURL, basePath, accountId, Authorization }
+  return { baseURL, accountId, Authorization }
 }
 
 function init (): AxiosInstance {
