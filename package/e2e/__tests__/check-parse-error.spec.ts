f4c842c29938ada188c9e6a0253578aa93a2cbc2
Umut Uzgur <umutuzgur@gmail.com>
2023-01-20 10:09:41 +0100

    fix: update the test [sc-0]

--- package/e2e/__tests__/check-parse-error.spec.ts
+++ package/e2e/__tests__/check-parse-error.spec.ts
@@ -12,6 +12,6 @@ describe('check parse error', () => {
     })
     const toAbsolutePath = (filename: string) => path.join(__dirname, 'fixtures', 'check-parse-error', filename)
 
-    expect(result.stderr.trim().replace(/(\r\n|\n|\r)/gm, '')).toContain(toAbsolutePath('entrypoint.js'))
+    expect(result.stderr.replace(/(\r\n|\n|\r|\s+)/gm, '')).toContain(toAbsolutePath('entrypoint.js'))
   })
 })
