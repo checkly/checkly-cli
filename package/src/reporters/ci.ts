9714d0d67f0eedf6d4c250e1a27371b0742b97f6
tnolet <tim@checklyhq.com>
2023-01-27 23:31:18 +0100

    feat: adds --list flag

--- package/src/reporters/ci.ts
+++ package/src/reporters/ci.ts
@@ -4,6 +4,11 @@ import AbstractListReporter from './abstract-list'
 import { formatCheckTitle, formatCheckResult, CheckStatus } from './util'
 
 export default class CiReporter extends AbstractListReporter {
+  onBeginStatic () {
+    console.log('\nListing all checks:\n')
+    this._printSummary({ skipCheckCount: true })
+  }
+
   onBegin () {
     console.log(`\nRunning ${this.numChecks} checks in ${this._runLocationString()}:\n`)
     this._printSummary({ skipCheckCount: true })
