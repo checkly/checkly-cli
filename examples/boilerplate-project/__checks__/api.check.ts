b468c7c0c0eda4e0e41f38905237090c5b150a2b
tnolet <tim@checklyhq.com>
2023-02-03 14:30:11 +0100

    feat: adds boilerplate example

---
+++ examples/boilerplate-project/__checks__/api.check.ts
@@ -0,0 +1,18 @@
+import { ApiCheck } from '@checkly/cli/dist/constructs'
+
+new ApiCheck('homepage-api-check-1', {
+  name: 'Homepage - fetch stats',
+  alertChannels: [],
+  degradedResponseTime: 10000,
+  maxResponseTime: 20000,
+  request: {
+    url: 'https://api.checklyhq.com/public-stats',
+    method: 'GET',
+    followRedirects: true,
+    skipSsl: false,
+    assertions: [
+      { source: 'STATUS_CODE', property: '', comparison: 'EQUALS', target: '200', regex: '' },
+      { source: 'JSON_BODY', regex: '', property: '$.apiCheckResults', comparison: 'GREATER_THAN', target: '0' }
+    ],
+  }
+})
---
+++ examples/boilerplate-project/checkly.config.ts
@@ -0,0 +1,19 @@
+const config = {
+  projectName: 'Boilerplate Project',
+  logicalId: 'boilerplate-project',
+  repoUrl: 'https://github.com/checkly/checkly-cli',
+  checks: {
+    locations: [ 'us-east-1', 'eu-west-1' ],
+    tags: ['mac'],
+    runtimeId: '2022.10',
+    checkMatch: '**/*.check.ts',
+    browserChecks: {
+      testMatch: '**/__checks__/*.spec.ts', // this matches any Playwright spec-files and automagically creates a Browser check
+    },
+  },
+  cli: {
+    runLocation: 'eu-west-1',
+  },
+}
+
+module.exports = config
