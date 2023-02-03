3ef6530def5e9203201058f8e9e0d154d8642370
tnolet <tim@checklyhq.com>
2023-02-03 15:02:54 +0100

    chore: remove pre-compiled JS files

--- examples/simple-project/src/defaults.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaults = void 0;
-exports.defaults = {
-    pageUrl: process.env.ENVIRONMENT_URL || 'https://checklyhq.com',
-    playwright: {
-        viewportSize: { width: 1920, height: 1080 },
-    },
-};
