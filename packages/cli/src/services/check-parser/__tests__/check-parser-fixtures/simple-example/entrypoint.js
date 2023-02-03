eb2407cc61bea2347e87909a6dd7cb97cccc2243
Umut Uzgur <umutuzgur@gmail.com>
2023-02-03 12:41:07 +0100

    feat: support barrel files for ts and js [sc-0] (#471)

--- packages/cli/src/services/check-parser/__tests__/check-parser-fixtures/simple-example/entrypoint.js
+++ packages/cli/src/services/check-parser/__tests__/check-parser-fixtures/simple-example/entrypoint.js
@@ -1,6 +1,8 @@
 // Require two other files
 const dep1 = require('./dep1')
 const dep2 = require('./dep2')
+const moduleImport = require('./module')
+const modulePackage = require('./module-package')
 
 // Require an npm package
 const axios = require('axios')
---
+++ packages/cli/src/services/check-parser/__tests__/check-parser-fixtures/typescript-example/module/index.ts
@@ -0,0 +1,3 @@
+export function moduleImport () {
+  return true
+}
