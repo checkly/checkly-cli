b5f61c6f162984f31397b129676cb14539ac20b7
Chris Lample <chris@checklyhq.com>
2023-02-08 17:35:08 +0100

    feat: add a fluent API for buiilding assertions

--- packages/cli/src/constructs/api-check.ts
+++ packages/cli/src/constructs/api-check.ts
@@ -2,12 +2,166 @@ import { Check, CheckProps } from './check'
 import { HttpHeader } from './http-header'
 import { Session } from './project'
 import { QueryParam } from './query-param'
+
+enum AssertionSource {
+  STATUS_CODE = 'STATUS_CODE',
+  JSON_BODY = 'JSON_BODY',
+  HEADERS = 'HEADERS',
+  TEXT_BODY = 'TEXT_BODY',
+  RESPONSE_TIME = 'RESPONSE_TIME',
+}
+
+enum AssertionComparison {
+  EQUALS = 'EQUALS',
+  NOT_EQUALS = 'NOT_EQUALS',
+  HAS_KEY = 'HAS_KEY',
+  NOT_HAS_KEY = 'NOT_HAS_KEY',
+  HAS_VALUE = 'HAS_VALUE',
+  NOT_HAS_VALUE = 'NOT_HAS_VALUE',
+  IS_EMPTY = 'IS_EMPTY',
+  NOT_EMPTY = 'NOT_EMPTY',
+  GREATER_THAN = 'GREATER_THAN',
+  LESS_THAN = 'LESS_THAN',
+  CONTAINS = 'CONTAINS',
+  NOT_CONTAINS = 'NOT_CONTAINS',
+  IS_NULL = 'IS_NULL',
+  NOT_NULL = 'NOT_NULL',
+}
+
 export interface Assertion {
   source: string,
   property: string,
   comparison: string,
-  target: string
-  regex: string
+  target: string,
+  regex: string|null,
+}
+
+export class AssertionBuilder {
+  static statusCode () {
+    return new NumericAssertionBuilder(AssertionSource.STATUS_CODE)
+  }
+
+  static jsonBody (property?: string) {
+    return new GeneralAssertionBuilder(AssertionSource.JSON_BODY, property)
+  }
+
+  static headers (property?: string, regex?: string) {
+    return new GeneralAssertionBuilder(AssertionSource.HEADERS, property, regex)
+  }
+
+  static textBody (property?: string) {
+    return new GeneralAssertionBuilder(AssertionSource.TEXT_BODY, property)
+  }
+
+  static responseTme () {
+    return new NumericAssertionBuilder(AssertionSource.RESPONSE_TIME)
+  }
+}
+
+class NumericAssertionBuilder {
+  source: AssertionSource
+  constructor (source: AssertionSource) {
+    this.source = source
+  }
+
+  equals (target: number): Assertion {
+    return this.toAssertion(AssertionComparison.EQUALS, target)
+  }
+
+  notEquals (target: number): Assertion {
+    return this.toAssertion(AssertionComparison.NOT_EQUALS, target)
+  }
+
+  lessThan (target: number): Assertion {
+    return this.toAssertion(AssertionComparison.LESS_THAN, target)
+  }
+
+  greaterThan (target: number): Assertion {
+    return this.toAssertion(AssertionComparison.GREATER_THAN, target)
+  }
+
+  /** @private */
+  private toAssertion (comparison: AssertionComparison, target: number): Assertion {
+    return { source: this.source, comparison, property: '', target: target.toString(), regex: null }
+  }
+}
+
+class GeneralAssertionBuilder {
+  source: AssertionSource
+  property?: string
+  regex?: string
+  constructor (source: AssertionSource, property?: string, regex?: string) {
+    this.source = source
+    this.property = property
+    this.regex = regex
+  }
+
+  equals (target: string|number|boolean): Assertion {
+    return this.toAssertion(AssertionComparison.EQUALS, target)
+  }
+
+  notEquals (target: string|number|boolean): Assertion {
+    return this.toAssertion(AssertionComparison.NOT_EQUALS, target)
+  }
+
+  hasKey (target: string): Assertion {
+    return this.toAssertion(AssertionComparison.HAS_KEY, target)
+  }
+
+  notHasKey (target: string): Assertion {
+    return this.toAssertion(AssertionComparison.NOT_HAS_KEY, target)
+  }
+
+  hasValue (target: string|number|boolean): Assertion {
+    return this.toAssertion(AssertionComparison.HAS_VALUE, target)
+  }
+
+  notHasValue (target: string|number|boolean): Assertion {
+    return this.toAssertion(AssertionComparison.NOT_HAS_VALUE, target)
+  }
+
+  isEmpty () {
+    return this.toAssertion(AssertionComparison.IS_EMPTY)
+  }
+
+  notEmpty () {
+    return this.toAssertion(AssertionComparison.NOT_EMPTY)
+  }
+
+  lessThan (target: string|number|boolean): Assertion {
+    return this.toAssertion(AssertionComparison.LESS_THAN, target)
+  }
+
+  greaterThan (target: string|number|boolean): Assertion {
+    return this.toAssertion(AssertionComparison.GREATER_THAN, target)
+  }
+
+  contains (target: string): Assertion {
+    return this.toAssertion(AssertionComparison.CONTAINS, target)
+  }
+
+  notContains (target: string): Assertion {
+    return this.toAssertion(AssertionComparison.CONTAINS, target)
+  }
+
+  isNull () {
+    return this.toAssertion(AssertionComparison.IS_NULL)
+  }
+
+  isNotNull () {
+    return this.toAssertion(AssertionComparison.NOT_NULL)
+  }
+
+  /** @private */
+  private toAssertion (comparison: AssertionComparison, target?: string|number|boolean): Assertion {
+    return {
+      source: this.source,
+      comparison,
+      property: this.property ?? '',
+      target: target !== undefined ? target.toString() : '',
+      regex: this.regex ?? null,
+    }
+  }
 }
 
 enum BodyType {
