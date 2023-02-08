f6196cf147f697b958e158f649d697602441b5ae
Nahuel Alejandro Ramos <nahuel@checklyhq.com>
2023-02-08 16:37:45 -0300

    feat: add logicalId validation [sc-0]

--- packages/cli/src/constructs/construct.ts
+++ packages/cli/src/constructs/construct.ts
@@ -1,10 +1,14 @@
 import { Session } from './project'
 import { Ref } from './ref'
+import { ValidationError } from './validator-error'
 
 export abstract class Construct {
   type: string
   logicalId: string
   constructor (type: string, logicalId: string) {
+    if (!/^[A-Za-z0-9_\-/#.]+$/.test(logicalId)) {
+      throw new ValidationError('The `logicalId` must includes only allowed characters [A-Za-z0-9_-/#]')
+    }
     Session.validateCreateConstruct(this)
     this.logicalId = logicalId
     this.type = type
