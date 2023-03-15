ee02f2ca592a28b313ce6f2568621d7ff6d759eb
Stefan Judis <stefanjudis@gmail.com>
2023-03-15 10:41:45 +0100

    fix: clean up TS warnings if PWT types are available (#585)

--- examples/advanced-project/src/__checks__/homepage.spec.ts
+++ examples/advanced-project/src/__checks__/homepage.spec.ts
@@ -6,7 +6,7 @@ test('Checkly Homepage', async ({ page }) => {
   await page.setViewportSize(defaults.playwright.viewportSize)
   const response = await page.goto(defaults.pageUrl)
 
-  expect(response.status()).toBeLessThan(400)
+  expect(response?.status()).toBeLessThan(400)
   await expect(page).toHaveTitle(/Build and Run Synthetics That Scale/)
   await page.screenshot({ path: 'homepage.jpg' })
 })
