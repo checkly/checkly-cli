3be24b57301810598ffb8684f4ec205a7de21b03
Nahuel Alejandro Ramos <nahuel@checklyhq.com>
2023-03-21 11:31:34 -0300

    feat: add E2E test using skipSSL [gh-597]

--- packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
+++ packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
@@ -17,3 +17,19 @@ const apiCheck = new ApiCheck('homepage-api-check-1', {
     ],
   },
 })
+
+const skipSslApiCheck = new ApiCheck('ssl-api-check-1', {
+  name: 'Skip SSL Check',
+  alertChannels: [slackChannel, webhookChannel],
+  degradedResponseTime: 10000,
+  maxResponseTime: 20000,
+  request: {
+    url: 'https://self-signed.badssl.com/',
+    method: 'GET',
+    followRedirects: true,
+    skipSSL: true,
+    assertions: [
+      AssertionBuilder.statusCode().equals(200),
+    ],
+  },
+})
