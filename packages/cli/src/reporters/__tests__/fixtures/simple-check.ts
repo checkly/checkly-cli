d16ad69d11ad9ac6ac85a7a10977e54e6bf8274f
Tim Nolet <tnolet@users.noreply.github.com>
2023-03-24 14:57:52 +0100

    582 bug no log information is provided for failing api check setup scripts 2 (#608)
    * feat: logs HTTP request and setup / teardown logs. Tweaks log format
    
    * fix: prints name of check when running --verbose
    
    * test: adds tests for util format functions
    
    * test: updates snapshot
    
    * test: hard remove snapshots
    
    * test: creates brand new snapshots file
    
    * test: adds stripping ANSI characters from terminal output for snapshot testing across platforms
    
    * test: fix timezone in test

---
+++ packages/cli/src/reporters/__tests__/fixtures/simple-check.ts
@@ -0,0 +1,6 @@
+export const simpleCheckFixture: object = {
+  name: 'Test Check',
+  startedAt: '2023-03-24T03:17:18.981Z',
+  stoppedAt: '2023-03-24T03:17:29.708Z',
+  sourceFile: '/test/test-file.check.ts',
+}
