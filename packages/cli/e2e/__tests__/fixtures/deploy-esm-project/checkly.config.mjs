3fbb3f9b3f36146be8200c29b9fe40ee9afac766
Umut Uzgur <umutuzgur@gmail.com>
2023-04-05 16:27:26 +0200

    feat: add mjs support to the cli [gh-616]

--- packages/cli/e2e/__tests__/fixtures/deploy-esm-project/checkly.config.mjs
+++ packages/cli/e2e/__tests__/fixtures/deploy-esm-project/checkly.config.mjs
@@ -2,5 +2,8 @@ const config = {
   projectName: 'Deploy Project',
   logicalId: process.env.PROJECT_LOGICAL_ID,
   repoUrl: 'https://github.com/checkly/checkly-cli',
+  checks: {
+    checkMatch: '**/*.check.{js,mjs,ts}',
+  },
 }
 export default config
