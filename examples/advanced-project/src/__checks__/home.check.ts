eeb9f1f7252b6bf4278d2c4c9923c048526d519f
Manel Fourati <manel@checklyhq.com>
2023-04-05 15:04:49 +0200

    fix: remove typo from `home.check.ts` description [sc-00] (#626)

--- examples/advanced-project/src/__checks__/home.check.ts
+++ examples/advanced-project/src/__checks__/home.check.ts
@@ -7,7 +7,7 @@ const alertChannels = [smsChannel, emailChannel]
 
 /*
 * In this example, we bundle all basic checks needed to check the Checkly homepage. We explicitly define the Browser
-* check here, instead of using generating a default based on a .spec.js file. This allows us to override the check configuration.
+* check here, instead of using a default based on a .spec.js file. This allows us to override the check configuration.
 * We can also add more checks into one file, in this case to cover a specific API call needed to hydrate the homepage.
 */
 
