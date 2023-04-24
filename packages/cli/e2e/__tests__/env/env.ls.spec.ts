b17916fb71fef1db192761a1ef2fb8d463355274
Chris Lample <chris@checklyhq.com>
2023-04-24 15:03:39 +0200

    feat: add commands for managing account environment variables (#647)
    Co-authored-by: Hannes Lenke <hannes@lenke.at>
    Co-authored-by: Nahuel Alejandro Ramos <nahuel@checklyhq.com>

---
+++ packages/cli/e2e/__tests__/env/env.ls.spec.ts
@@ -0,0 +1,37 @@
+// create test for checkly env ls
+import * as path from 'path'
+import * as config from 'config'
+import { runChecklyCli } from '../../run-checkly'
+
+describe('checkly env ls', () => {
+  // before testing add a new environment variable vi checkly env add test true
+  beforeAll(() => {
+    runChecklyCli({
+      args: ['env', 'add', 'testenvvarsls', 'testvalue'],
+      apiKey: config.get('apiKey'),
+      accountId: config.get('accountId'),
+      directory: path.join(__dirname, '../fixtures/check-parse-error'),
+    })
+  })
+
+  // after testing remove the environment variable vi checkly env rm test
+  afterAll(() => {
+    runChecklyCli({
+      args: ['env', 'rm', 'testenvvarsls', '--force'],
+      apiKey: config.get('apiKey'),
+      accountId: config.get('accountId'),
+      directory: path.join(__dirname, '../fixtures/check-parse-error'),
+    })
+  })
+
+  it('should list all environment variables', () => {
+    const result = runChecklyCli({
+      args: ['env', 'ls'],
+      apiKey: config.get('apiKey'),
+      accountId: config.get('accountId'),
+      directory: path.join(__dirname, '../fixtures/check-parse-error'),
+    })
+    // expect that 'testenvvars' is in the output
+    expect(result.stdout).toContain('testenvvarsls')
+  })
+})
