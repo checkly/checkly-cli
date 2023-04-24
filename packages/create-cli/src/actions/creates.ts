3d1370d44bd32a89df3824d9b96737c4cff3d794
Maxi <maxi@checklyhq.com>
2023-04-24 09:47:37 +0100

    feat: copy + normalize url [sc-00]

--- packages/create-cli/src/actions/creates.ts
+++ packages/create-cli/src/actions/creates.ts
@@ -1,5 +1,18 @@
 import * as fs from 'fs'
 import prompts from 'prompts'
+import normalizeUrl from 'normalize-url'
+
+// Default Playwright-based Browser Check
+const defaultBrowserCheck = `import { test, expect } from '@playwright/test'
+
+// You can override the default Playwright test timeout of 30s
+// test.setTimeout(60_000);
+
+test('Custom Browser Check', async ({ page }) => {
+  const response = await page.goto('URL_TO_CHECK')
+  expect(response?.status()).toBeLessThan(400)
+  await page.screenshot({ path: 'screenshot.jpg' })
+})`
 
 export async function createCustomBrowserCheck (
   { onCancel }: { onCancel: () => void },
@@ -7,7 +20,7 @@ export async function createCustomBrowserCheck (
   const createInitialBrowserCheckResponse = await prompts({
     type: 'confirm',
     name: 'value',
-    message: 'Would you like to create a custom Browser Check?',
+    message: 'Would you like to create a custom Playwright-based Browser Check to check a URL?',
     initial: true,
   },
   { onCancel },
@@ -17,20 +30,11 @@ export async function createCustomBrowserCheck (
     const userWebsiteResponse = await prompts({
       type: 'text',
       name: 'url',
-      message: 'Could you provide your website homepage URL?',
+      message: 'Please provide the URL of the site you want to check.',
     },
     { onCancel },
     )
 
-    fs.writeFileSync('./__checks__/custom.spec.ts', `import { test, expect } from '@playwright/test'
-
-// You can override the default Playwright test timeout of 30s
-// test.setTimeout(60_000);
-
-test('Custom Browser Check', async ({ page }) => {
-  const response = await page.goto('${userWebsiteResponse.url}')
-  expect(response?.status()).toBeLessThan(400)
-  await page.screenshot({ path: 'screenshot.jpg' })
-})`)
+    fs.writeFileSync('./__checks__/custom.spec.ts', defaultBrowserCheck.replace(/URL_TO_CHECK/i, normalizeUrl(userWebsiteResponse.url)))
   }
 }
