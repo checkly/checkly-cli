d026e13987842b9ca6dfebd073abc9d976fe644e
Umut Uzgur <umutuzgur@gmail.com>
2023-04-25 10:42:50 +0200

    chore: convert functions to full static fields [sc-0]

--- packages/cli/src/constructs/frequency.ts
+++ packages/cli/src/constructs/frequency.ts
@@ -1,38 +1,26 @@
-const HIGH_FREQ_OFFSETS = [10, 20, 30]
-type Second = typeof HIGH_FREQ_OFFSETS[number]
-
-const MIN_FREQS = [1, 2, 5, 10, 15, 30]
-type Minute = typeof MIN_FREQS[number]
-
-const HOUR_FREQS = [1, 2, 3, 6, 12, 24]
-type Hour = typeof HOUR_FREQS[number]
-
 export class Frequency {
+  static EVERY_10S = new Frequency(0, 10)
+  static EVERY_20S = new Frequency(0, 20)
+  static EVERY_30S = new Frequency(0, 30)
+
+  static EVERY_1M = new Frequency(1)
+  static EVERY_2M = new Frequency(2)
+  static EVERY_5M = new Frequency(5)
+  static EVERY_10M = new Frequency(10)
+  static EVERY_15M = new Frequency(15)
+  static EVERY_30M = new Frequency(30)
+
+  static EVERY_1H = new Frequency(1 * 60)
+  static EVERY_2H = new Frequency(2 * 60)
+  static EVERY_3M = new Frequency(3 * 60)
+  static EVERY_6H = new Frequency(6 * 60)
+  static EVERY_12H = new Frequency(12 * 60)
+  static EVERY_24H = new Frequency(24 * 60)
+
   frequency: number
   frequencyOffset?: number
   private constructor (frequency: number, frequencyOffset?: number) {
     this.frequency = frequency
     this.frequencyOffset = frequencyOffset
-  }
-
-  everySecond (freq: Second) {
-    if (!HIGH_FREQ_OFFSETS.includes(freq)) {
-      throw new Error(`Unsupproted freq ${freq}`)
-    }
-    return new Frequency(0, freq)
-  }
-
-  everyMinute (freq: Minute) {
-    if (!MIN_FREQS.includes(freq)) {
-      throw new Error(`Unsupproted freq ${freq}`)
-    }
-    return new Frequency(freq)
-  }
-
-  everyHour (freq: Hour) {
-    if (!HOUR_FREQS.includes(freq)) {
-      throw new Error(`Unsupproted freq ${freq}`)
-    }
-    return new Frequency(freq * 60)
   }
 }
