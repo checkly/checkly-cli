85e2ad1bc90253f183a51318c23bc5a8d5690d5c
Chris Lample <chris@checklyhq.com>
2023-05-03 16:03:20 +0200

    test: add integration test for checkly trigger (#684)

---
+++ packages/cli/e2e/__tests__/fixtures/trigger-project/checks.check.ts
@@ -0,0 +1,51 @@
+import { ApiCheck, CheckGroup } from 'checkly/constructs'
+
+const executionId = process.env.EXECUTION_ID!
+
+const prodBackendGroup = new CheckGroup('prod-backend-group', {
+  name: 'Production Backend Group (Trigger Test)',
+  locations: ['eu-central-1'],
+  tags: ['production', 'backend', executionId, 'additional-tag'],
+})
+
+new ApiCheck('prod-backend-check', {
+  name: 'Prod Backend Check (Trigger Test)',
+  activated: false,
+  request: {
+    url: 'https://www.google.com',
+    method: 'GET',
+    followRedirects: false,
+    skipSSL: false,
+    assertions: [],
+  },
+  group: prodBackendGroup,
+})
+
+new ApiCheck('prod-frontend-check', {
+  name: 'Prod Frontend Check (Trigger Test)',
+  activated: false,
+  request: {
+    url: 'https://www.google.com',
+    method: 'GET',
+    followRedirects: false,
+    skipSSL: false,
+    assertions: [],
+  },
+  tags: ['production', 'frontend', executionId, 'another-tag'],
+  // Log the secret environment variable passed to `checkly trigger`
+  setupScript: { content: "console.log(process.env.SECRET_ENV)" },
+})
+
+new ApiCheck('staging-backend-check', {
+  name: 'Staging Backend Check (Trigger Test)',
+  activated: false,
+  request: {
+    url: 'https://www.google.com',
+    method: 'GET',
+    followRedirects: false,
+    skipSSL: false,
+    assertions: [],
+  },
+  tags: ['staging', 'backend', executionId],
+})
+
