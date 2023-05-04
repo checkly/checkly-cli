f2b722eb9782d401eca05ea6e97a3a77bf39c5f3
Umut Uzgur <umutuzgur@gmail.com>
2023-05-04 14:22:59 +0100

    chore: rename env var and move api url to the config [gh-0] (#694)

--- packages/cli/src/services/socket-client.ts
+++ packages/cli/src/services/socket-client.ts
@@ -3,9 +3,9 @@ import config from '../services/config'
 
 export class SocketClient {
   static connect (): Promise<mqtt.AsyncMqttClient> {
-    const url = config.getMqttUrl()!
-    const accountId = config.getAccountId()!
-    const apiKey = config.getApiKey()!
+    const url = config.getMqttUrl()
+    const accountId = config.getAccountId()
+    const apiKey = config.getApiKey()
     const options = {
       reconnectPeriod: 0,
       username: accountId,
