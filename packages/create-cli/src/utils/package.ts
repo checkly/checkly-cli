cb02beb614ed2879da635238cc86a4aed2e722fd
Maxi <maxi@checklyhq.com>
2023-05-08 17:39:24 +0100

    fix: detect if package.json exists [sc-15315]

--- packages/create-cli/src/utils/package.ts
+++ packages/create-cli/src/utils/package.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs'
+import path from 'path'
 
 export interface PackageJson {
   name: string;
@@ -8,7 +9,7 @@ export interface PackageJson {
 }
 
 export function hasPackageJsonFile () {
-  return fs.existsSync('./package.json')
+  return fs.existsSync(path.join(process.cwd(), './package.json'))
 }
 
 export function readPackageJson (): PackageJson {
