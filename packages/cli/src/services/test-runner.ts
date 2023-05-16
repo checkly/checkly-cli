c1332a9b30bf68c37e45db826651db99b69f71f2
Umut Uzgur <umutuzgur@gmail.com>
2023-05-16 12:24:32 +0100

    feat: send file path to the BE [gh-0] (#716)

--- packages/cli/src/services/test-runner.ts
+++ packages/cli/src/services/test-runner.ts
@@ -48,6 +48,7 @@ export default class TestRunner extends AbstractCheckRunner {
       groupId: undefined,
       sourceInfo: { checkRunSuiteId, checkRunId },
       logicalId: check.logicalId,
+      filePath: check.getSourceFile(),
     }))
     try {
       if (!checkRunJobs.length) {
