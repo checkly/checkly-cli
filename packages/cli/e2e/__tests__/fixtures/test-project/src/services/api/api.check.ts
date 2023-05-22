ff870747c1df29085e58d3fcd06cd997afe61dba
Manel <manel.fourati@esprit.tn>
2023-05-22 20:52:52 +0200

    refactor: replace `/public-stats` usage [sc-15393]

--- packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
+++ packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
@@ -1,23 +1,6 @@
 import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
 import { slackChannel, webhookChannel } from '../../alert-channels'
 
-const apiCheck = new ApiCheck('homepage-api-check-1', {
-  name: 'Public Stats',
-  alertChannels: [slackChannel, webhookChannel],
-  degradedResponseTime: 10000,
-  maxResponseTime: 20000,
-  request: {
-    url: 'https://api.checklyhq.com/public-stats',
-    method: 'GET',
-    followRedirects: true,
-    skipSSL: false,
-    assertions: [
-      AssertionBuilder.statusCode().equals(200),
-      AssertionBuilder.jsonBody('$.apiCheckResults').greaterThan(0),
-    ],
-  },
-})
-
 const skipSslApiCheck = new ApiCheck('ssl-api-check-1', {
   name: 'Skip SSL Check',
   alertChannels: [slackChannel, webhookChannel],
