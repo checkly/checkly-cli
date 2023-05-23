f99ae5a120a19156783a9a9c92a377c2ea9c4a9d
Manel <manel.fourati@esprit.tn>
2023-05-23 15:00:52 +0200

    refactor: re-add `public stats`apiCheck, replace it with `runtimes`  [sc-15393]

--- packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
+++ packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
@@ -1,6 +1,23 @@
 import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
 import { slackChannel, webhookChannel } from '../../alert-channels'
 
+const apiCheck = new ApiCheck('homepage-api-check-1', {
+  name: 'Runtimes',
+  alertChannels: [slackChannel, webhookChannel],
+  degradedResponseTime: 10000,
+  maxResponseTime: 20000,
+  request: {
+    url: 'https://api.checklyhq.com/v1/runtimes',
+    method: 'GET',
+    followRedirects: true,
+    skipSSL: false,
+    assertions: [
+      AssertionBuilder.statusCode().equals(200),
+      AssertionBuilder.jsonBody('$.length').greaterThan(0),
+    ],
+  },
+})
+
 const skipSslApiCheck = new ApiCheck('ssl-api-check-1', {
   name: 'Skip SSL Check',
   alertChannels: [slackChannel, webhookChannel],
