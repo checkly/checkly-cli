95cefeb84f8b0393bbfa4d1965b4dcd34a0cca45
Manel Fourati <manel.fourati@esprit.tn>
2023-05-23 15:11:05 +0200

    refactor: replace `/public-stats` usage [sc-15393] (#728)
    * refactor: replace `/public-stats` usage [sc-15393]
    
    * refactor: re-add `public stats`apiCheck, replace it with `runtimes`  [sc-15393]

--- packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/typo.checkz.ts
+++ packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/typo.checkz.ts
@@ -3,7 +3,7 @@ import { ApiCheck } from 'checkly/constructs'
 const apiCheck = new ApiCheck('not-included-typo-api-check-1', {
   name: 'File extension type example',
   request: {
-    url: 'https://api.checklyhq.com/public-stats',
+    url: 'https://api.checklyhq.com/v1/runtimes',
     method: 'GET',
     assertions: [],
   },
