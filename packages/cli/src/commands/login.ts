f1df07759947085d47bc1d15b6235050d9683dfe
Umut Uzgur <umutuzgur@gmail.com>
2023-05-25 15:12:13 +0200

    fix: use the write input type for account names [gh-732]

--- packages/cli/src/commands/login.ts
+++ packages/cli/src/commands/login.ts
@@ -16,7 +16,7 @@ const selectAccount = async (accounts: Array<Account>): Promise<Account> => {
   const { accountName } = await prompts({
     name: 'accountName',
     type: 'list',
-    choices: accounts.map(a => ({ title: a.name })),
+    choices: accounts.map(({ name }) => ({ title: name, value: name })),
     message: 'Which account do you want to use?',
   })
 
