95fb19e7abf3eb5f1fb2146a7dad09d652879342
Chris Lample <chris@checklyhq.com>
2023-06-09 12:08:47 +0200

    fix: lazily initialize config file (#762)

--- packages/cli/src/services/config.ts
+++ packages/cli/src/services/config.ts
@@ -19,26 +19,38 @@ enum Env {
   local = 'local',
 }
 
-const config = {
-  auth: new Conf({
-    projectName: '@checkly/cli',
-    configName: 'auth',
-    projectSuffix,
-    // @ts-ignore
-    schema: authSchema,
-  }),
-  data: new Conf({
-    projectName: '@checkly/cli',
-    configName: 'config',
-    projectSuffix,
-    // @ts-ignore
-    schema: dataSchema,
-  }),
+class ChecklyConfig {
+  private _auth?: Conf<{ apiKey: unknown }>
+  private _data?: Conf<{ accountId: unknown, accountName: unknown }>
+
+  // Accessing auth or data will cause a config file to be created.
+  // We should avoid doing this unless absolutely necessary, since this operation can fail due to file permissions.
+  get auth () {
+    // Create this._auth lazily
+    return this._auth ?? (this._auth = new Conf({
+      projectName: '@checkly/cli',
+      configName: 'auth',
+      projectSuffix,
+      // @ts-ignore
+      schema: authSchema,
+    }))
+  }
+
+  get data () {
+    // Create this._data lazily
+    return this._data ?? (this._data = new Conf({
+      projectName: '@checkly/cli',
+      configName: 'config',
+      projectSuffix,
+      // @ts-ignore
+      schema: dataSchema,
+    }))
+  }
 
   clear () {
     this.auth.clear()
     this.data.clear()
-  },
+  }
 
   getEnv (): Env {
     const environments = ['production', 'development', 'staging', 'local']
@@ -49,21 +61,21 @@ const config = {
     }
 
     return env as Env
-  },
+  }
 
   getApiKey (): string {
     return process.env.CHECKLY_API_KEY || this.auth.get<string>('apiKey') as string || ''
-  },
+  }
 
   getAccountId (): string {
     return process.env.CHECKLY_ACCOUNT_ID || this.data.get<string>('accountId') as string || ''
-  },
+  }
 
   hasEnvVarsConfigured (): boolean {
     const apiKey = process.env.CHECKLY_API_KEY || ''
     const accoundId = process.env.CHECKLY_ACCOUNT_ID || ''
     return apiKey !== '' || accoundId !== ''
-  },
+  }
 
   getApiUrl (): string {
     const environments = {
@@ -73,7 +85,7 @@ const config = {
       production: 'https://api.checklyhq.com',
     }
     return environments[this.getEnv()]!
-  },
+  }
 
   getMqttUrl (): string {
     const environments = {
@@ -83,14 +95,15 @@ const config = {
       production: 'wss://events.checklyhq.com',
     }
     return environments[this.getEnv()]!
-  },
+  }
 
   hasValidCredentials (): boolean {
     if (this.getApiKey() !== '' && this.getAccountId() !== '') {
       return true
     }
     return false
-  },
+  }
 }
 
+const config = new ChecklyConfig()
 export default config
