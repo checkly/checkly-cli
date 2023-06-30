fc57d693c3c82c5b7c3f08a2cfd46b8b63c38c40
Nahuel Alejandro Ramos <nahuel@checklyhq.com>
2023-06-30 10:06:22 -0300

    test: adjust waiting prompt [gh-0]

--- packages/cli/e2e/__tests__/switch.spec.ts
+++ packages/cli/e2e/__tests__/switch.spec.ts
@@ -1,6 +1,6 @@
 import { runChecklyCliForSwitch } from '../run-checkly'
 import * as config from 'config'
-
+jest.setTimeout(1000000)
 describe('switch', () => {
   it('should switch between user accounts', async () => {
     const { status, stdout } = await runChecklyCliForSwitch({
@@ -8,7 +8,7 @@ describe('switch', () => {
       apiKey: config.get('apiKey'),
       accountId: config.get('accountId'),
     })
-    expect(stdout).toContain('Select a new Checkly account')
+    expect(stdout).toContain('Which account do you want to use?')
     expect(stdout).toContain('Account switched to')
     expect(status).toBe(0)
   })
