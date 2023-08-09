ad3637cc2a1fb77ab78e9f2774b089c3cb02e6be
Manel <manel.fourati@esprit.tn>
2023-08-09 14:31:23 +0200

    refactor: comment out heartbeat checks to prevent error on creation [sc-17054]

--- examples/advanced-project/src/__checks__/heartbeat.check.ts
+++ examples/advanced-project/src/__checks__/heartbeat.check.ts
@@ -1,9 +1,16 @@
 import { HeartbeatCheck } from 'checkly/constructs'
 
-new HeartbeatCheck('heartbeat-check-1', {
+// Heartbeat checks allow you to monitor jobs or recurring tasks.
+// This feature is only available on paid plans.
+// Upgrade your plan to start using it https://app.checklyhq.com/new-billing
+// If you're already on a paid plan, uncomment the following lines to create a heartbeat check.
+
+/* new HeartbeatCheck('heartbeat-check-1', {
     name: 'Send weekly newsletter job',
     period: 30,
     periodUnit: 'minutes',
     grace: 10,
     graceUnit: 'minutes',
 })
+ */
+
