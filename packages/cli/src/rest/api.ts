092f82c182f6b5b1ff1d732d6e29ed5b94589ac3
Nahuel Alejandro Ramos <nahuel@checklyhq.com>
2023-08-14 12:11:07 -0400

    chore: handle 408 error and show message [gh-824] (#825)
    * chore: handle 408 error and show message [gh-824]
    
    * chore: remove incomplete unit-test [gh-824]
    
    * test: remove runtimes e2e test [gh-824]
    
    * chore: rephrase error message [gh-824]

--- packages/cli/src/rest/api.ts
+++ packages/cli/src/rest/api.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios'
 import config from '../services/config'
 import { assignProxy } from '../services/util'
 import Accounts from './accounts'
@@ -46,26 +46,43 @@ export async function validateAuthentication (): Promise<void> {
   }
 }
 
+export function requestInterceptor (config: InternalAxiosRequestConfig) {
+  const { Authorization, accountId } = getDefaults()
+  if (Authorization && config.headers) {
+    config.headers.Authorization = Authorization
+  }
+
+  if (accountId && config.headers) {
+    config.headers['x-checkly-account'] = accountId
+  }
+
+  return config
+}
+
+export function responseErrorInterceptor (error: any) {
+  if (error.response?.status === 408) {
+    throw new Error('Encountered an error connecting to Checkly. ' +
+      'This can be triggered by a slow internet connection or a network with high packet loss.')
+  }
+  throw error
+}
+
 function init (): AxiosInstance {
   const { baseURL } = getDefaults()
   const axiosConf = assignProxy(baseURL, { baseURL })
 
   const api = axios.create(axiosConf)
 
-  api.interceptors.request.use(function (config) {
-    const { Authorization, accountId } = getDefaults()
-    if (Authorization && config.headers) {
-      config.headers.Authorization = Authorization
-    }
+  api.interceptors.request.use(requestInterceptor)
 
-    if (accountId && config.headers) {
-      config.headers['x-checkly-account'] = accountId
-    }
+  api.interceptors.response.use(
+    response => response,
+    responseErrorInterceptor,
+  )
 
-    return config
-  })
   return api
 }
+
 export const api = init()
 
 export const accounts = new Accounts(api)
