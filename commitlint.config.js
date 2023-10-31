93370ef4d1ec21a359e447f93dd917335241c821
Nico Domino <yo@ndo.dev>
2023-10-31 15:46:42 +0100

    fix: rm commitlint issue ref setting [sc-00] (#875)

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,11 +2,5 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 100],
-    'references-empty': [1, 'never'],
-  },
-  parserPreset: {
-    parserOpts: {
-      issuePrefixes: ['gh', 'sc'],
-    },
   },
 }
