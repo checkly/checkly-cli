f116cbc18defb3e80dcc24ec18ea391c612d38a2
Maxi <maxi@checklyhq.com>
2023-10-31 11:16:15 +0000

    feat: updates after comments [sc-00]

--- examples/advanced-project-js/src/__checks__/multi-step-spacex.check.js
+++ examples/advanced-project-js/src/__checks__/multi-step-spacex.check.js
@@ -5,9 +5,9 @@ import { smsChannel, emailChannel } from '../alert-channels'
 const alertChannels = [smsChannel, emailChannel]
 
 /*
-* In this example, we bundle all basic checks needed to check the Checkly homepage. We explicitly define the Browser
-* check here, instead of using a default based on a .spec.js file. This allows us to override the check configuration.
-* We can also add more checks into one file, in this case to cover a specific API call needed to hydrate the homepage.
+* In this example, we utilize the SpaceX public API to construct a series of chained requests, with the goal of confirming
+* that the capsules retrieved from the main endpoint match those obtained from the individual capsule endpoint.
+* Read more about in our documentation https://www.checklyhq.com/docs/multistep-checks/
 */
 
 // We can define multiple checks in a single *.check.js file.
