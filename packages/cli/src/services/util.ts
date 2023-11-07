8ff1565116f5277cfc52ec68d954e73790e6b7d7
Chris Lample <chris@checklyhq.com>
2023-11-07 10:04:32 +0100

    refactor: remove unused method (#881)

--- packages/cli/src/services/util.ts
+++ packages/cli/src/services/util.ts
@@ -25,24 +25,6 @@ export interface CiInformation {
   environment: string | null
 }
 
-// TODO: Remove this in favor of glob? It's unused.
-export async function walkDirectory (
-  directory: string,
-  ignoreDirectories: Set<string>,
-  callback: (filepath: string) => Promise<void>,
-): Promise<void> {
-  const files = await fs.readdir(directory)
-  for (const file of files.sort()) {
-    const filepath = path.join(directory, file)
-    const stats = await fs.stat(filepath)
-    if (stats.isDirectory() && !ignoreDirectories.has(file)) {
-      await walkDirectory(filepath, ignoreDirectories, callback)
-    } else {
-      await callback(filepath)
-    }
-  }
-}
-
 export function findFilesRecursively (directory: string, ignoredPaths: Array<string> = []) {
   if (!fsSync.statSync(directory, { throwIfNoEntry: false })?.isDirectory()) {
     return []
