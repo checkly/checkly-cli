83a72bbb3ddf8650b612f5d95a3bb5b863abbd6f
tnolet <tim@checklyhq.com>
2023-11-30 17:15:20 +0100

    test: fix the name

--- packages/cli/e2e/__tests__/fixtures/snapshot-project/snapshot-test.spec.ts
+++ packages/cli/e2e/__tests__/fixtures/snapshot-project/snapshot-test.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 
 test.use({ actionTimeout: 10000 })
 
-test('Danube Snapshot Test', async ({ page }) => {
+test('WWW Snapshot Test', async ({ page }) => {
   await page.goto('https://info.cern.ch/hypertext/WWW/TheProject.html')
   await expect(page).toHaveScreenshot({ maxDiffPixels: 10000 })
   console.log(process.env.SECRET_ENV)
