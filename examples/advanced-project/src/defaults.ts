e91c6f47f89239da7d9f444967ccb445e8dcfceb
tnolet <tim@checklyhq.com>
2024-01-25 15:46:03 +0100

    docs: adds playwrightConfig options to examples [sc-00]

--- examples/advanced-project/src/defaults.ts
+++
@@ -1,6 +0,0 @@
-export const defaults = {
-  pageUrl: process.env.ENVIRONMENT_URL || 'https://danube-web.shop', // the pageUrl can be replaced by urls from a preview / staging environment.
-  playwright: {
-    viewportSize: { width: 1920, height: 1080 },
-  },
-}
