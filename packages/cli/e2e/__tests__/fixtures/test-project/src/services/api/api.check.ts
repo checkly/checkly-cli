d3f7ffe95a21fc871e9086142477b0fb9aa833da
Simo Kinnunen <simo@checklyhq.com>
2024-11-28 02:13:15 +0900

    feat: use webhook secret in e2e tests
    Hardly a true test for whether the secret really gets set or not, but it's
    in line with the current tests.

--- packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
+++ packages/cli/e2e/__tests__/fixtures/test-project/src/services/api/api.check.ts
@@ -1,9 +1,13 @@
 import { ApiCheck, AssertionBuilder } from 'checkly/constructs'
-import { slackChannel, webhookChannel } from '../../alert-channels'
+import { slackChannel, webhookChannel, webhookChannelWithSecret } from '../../alert-channels'
 
 const apiCheck = new ApiCheck('homepage-api-check-1', {
   name: 'Runtimes',
-  alertChannels: [slackChannel, webhookChannel],
+  alertChannels: [
+    slackChannel,
+    webhookChannel,
+    webhookChannelWithSecret,
+  ],
   degradedResponseTime: 10000,
   maxResponseTime: 20000,
   request: {
