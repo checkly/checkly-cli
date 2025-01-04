ff8a8449f21b8656641b47f11403a0e8439dbe22
RedOctober117 <dbcahilly3@gmail.com>
2025-01-04 16:58:22 -0600

    fixes for all grievences but tests

--- packages/cli/src/constructs/msteams-alert-channel.ts
+++ packages/cli/src/constructs/msteams-alert-channel.ts
@@ -1,23 +1,15 @@
-import { AlertChannel, AlertChannelProps } from './alert-channel'
 import { Session } from './project'
+import { WebhookAlertChannel, WebhookAlertChannelProps } from './webhook-alert-channel'
 
-export interface MSTeamsAlertChannelProps extends AlertChannelProps {
+export interface MSTeamsAlertChannelProps extends WebhookAlertChannelProps {
   /**
    * The name of your MSTeams alert
    */
   name: string
   /**
-   * The name of your MSTeams alert channel.
-   */
-  channel_name: string
-  /**
    * The URL webhook to which to send updates.
    */
   url: string
-  /**
-   * Optional template for Teams Card.
-   */
-  template?: string
 }
 
 /**
@@ -27,11 +19,9 @@ export interface MSTeamsAlertChannelProps extends AlertChannelProps {
  *
  * This class make use of the Alert Channel endpoints.
  */
-export class MSTeamsAlertChannel extends AlertChannel {
+export class MSTeamsAlertChannel extends WebhookAlertChannel {
   name: string
-  channel_name: string
   url: string
-  template?: string
 
   /**
    * Constructs the MSTeams Alert Channel instance
@@ -41,71 +31,92 @@ export class MSTeamsAlertChannel extends AlertChannel {
    * Fix following url:
    * {@link https://checklyhq.com/docs/cli/constructs/#MSTeamsalertchannel Read more in the docs}
   */
-  constructor(logicalId: string, props: MSTeamsAlertChannelProps) {
+  constructor (logicalId: string, props: MSTeamsAlertChannelProps) {
     super(logicalId, props)
-    this.name = props.name;
-    this.channel_name = props.channel_name;
-    this.url = props.url;
+    this.name = props.name
+    this.url = props.url
     this.template = props.template || `{
   "type": "message",
   "attachments": [
     {
       "contentType": "application/vnd.microsoft.card.adaptive",
+      "contentUrl": null,
       "content": {
         "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
         "type": "AdaptiveCard",
         "version": "1.2",
         "body": [
           {
-            "size": "large",
-            "type": "TextBlock",
-            "text": "{{ALERT_TITLE}} has failed",
-            "wrap": true
-          },
-          {
-            "type": "TextBlock",
-            "text": "Response time: {{RESPONSE_TIME}}",
-            "wrap": true
-          },
-          {
-            "type": "TextBlock",
-            "text": "Location: {{RUN_LOCATION}}",
-            "wrap": true
-          },
-          {
-            "type": "TextBlock",
-            "text": "Timestamp: {{STARTED_AT}}",
-            "wrap": true
-          },
-          {
-            "type": "TextBlock",
-            "text": "Tags: {{TAGS}}",
-            "wrap": true
+            "type": "Container",
+            "items": [
+              {
+                "type": "TextBlock",
+                "text": "{{ALERT_TITLE}}",
+                "weight": "bolder",
+                "size": "medium"
+              },
+              {
+                "type": "ColumnSet",
+                "columns": [
+                  {
+                    "type": "Column",
+                    "width": "stretch",
+                    "items": [
+                      {
+                        "type": "TextBlock",
+                        "text": "Response time: {{RESPONSE_TIME}}ms",
+                        "wrap": true
+                      },
+                      {
+                        "type": "TextBlock",
+                        "text": "Location: {{RUN_LOCATION}}",
+                        "wrap": true
+                      },
+                      {
+                        "type": "TextBlock",
+                        "text": "Timestamp: {{STARTED_AT}}",
+                        "wrap": true
+                      },
+                      {{#if GROUP_NAME}}
+                      {
+                        "type": "TextBlock",
+                        "text": "Group: {{GROUP_NAME}}",
+                        "wrap": true
+                      },
+                      {{/if}}
+                      {
+                        "type": "TextBlock",
+                        "text": "Tags: {{#each TAGS}} {{this}} {{#unless @last}},{{/unless}} {{/each}}",
+                        "wrap": true
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
           }
         ],
         "actions": [
           {
             "type": "Action.OpenUrl",
-            "title": "Learn More",
-            "url": "https://adaptivecards.io"
+            "title": "View in Checkly",
+            "url": "{{RESULT_LINK}}"
           }
         ]
       }
     }
   ]
-}`;
+}`
     Session.registerConstruct(this)
   }
 
-  synthesize() {
+  synthesize () {
     return {
       ...super.synthesize(),
-      type: 'WEBHOOK',
+      type: 'WEBHOOK_MSTEAMS',
       config: {
         name: this.name,
-        channel_name: this.channel_name,
         url: this.url,
-        template: this.template,
       },
     }
   }
--- packages/cli/src/constructs/telegram-alert-channel.ts
+++ packages/cli/src/constructs/telegram-alert-channel.ts
@@ -1,11 +1,8 @@
-import { AlertChannel, AlertChannelProps } from './alert-channel'
-import { HttpHeader } from './http-header'
 import { HttpRequestMethod } from './api-check'
-import { QueryParam } from './query-param'
 import { Session } from './project'
 import { WebhookAlertChannel, WebhookAlertChannelProps } from './webhook-alert-channel'
 
-export interface TelegramAlertChannelProps extends AlertChannelProps {
+export interface TelegramAlertChannelProps extends WebhookAlertChannelProps {
     /**
      * The name of your Telegram alert
      */
@@ -13,16 +10,11 @@ export interface TelegramAlertChannelProps extends AlertChannelProps {
     /**
      * The chat id of your Telegram bot.
      */
-    chat_id: string
+    chatId: string
     /**
      * API token for your telegram bot.
      */
-    api_token: string
-
-    /**
-     * An optional template for use in the GET URL.
-     */
-    template?: string
+    apiToken: string
 }
 
 /**
@@ -30,17 +22,16 @@ export interface TelegramAlertChannelProps extends AlertChannelProps {
  *
  * @remarks
  *
- * This class make use of the Alert Channel endpoints.
+ * This class make use of the Webhook Alert Channel endpoints.
  */
-export class TelegramAlertChannel extends AlertChannel {
-    name: string
-    chat_id: string
-    api_token: string
-    template?: string
-    url: string
-    method: HttpRequestMethod
+export class TelegramAlertChannel extends WebhookAlertChannel {
+  name: string
+  chatId: string
+  apiToken: string
+  url: string
+  method: HttpRequestMethod
 
-    /**
+  /**
      * Constructs the Telegram Alert Channel instance
      *
      * @param logicalId unique project-scoped resource name identification
@@ -48,31 +39,28 @@ export class TelegramAlertChannel extends AlertChannel {
      * Fix following url:
      * {@link https://checklyhq.com/docs/cli/constructs/#Telegramalertchannel Read more in the docs}
     */
-    constructor(logicalId: string, props: TelegramAlertChannelProps) {
-        super(logicalId, props)
-        this.name = props.name;
-        this.chat_id = props.chat_id;
-        this.api_token = props.api_token;
-        this.template = props.template || `{{ALERT_TITLE}} at {{STARTED_AT}} in {{RUN_LOCATION}} ({{RESPONSE_TIME}}ms) Tags: {{#each TAGS}} {{this}} {{#unless @last}},{{/unless}} {{/each}} View check result: {{RESULT_LINK}}`;
-        this.url = `https://api.telegram.org/bot${props.api_token}/sendMessage?chat_id=${props.chat_id}&text=${this.template}`;
-        this.method = 'GET';
-        Session.registerConstruct(this)
-    }
+  constructor (logicalId: string, props: TelegramAlertChannelProps) {
+    super(logicalId, props)
+    this.name = props.name
+    this.chatId = props.chatId
+    this.apiToken = props.apiToken
+    this.template = props.template || `chat_id=${props.chatId}&parse_mode=HTML&text=<b>{{ALERT_TITLE}}</b> at {{STARTED_AT}} in {{RUN_LOCATION}} ({{RESPONSE_TIME}}ms)\nTags: {{#each TAGS}} <i><b>{{this}}</b></i> {{#unless @last}},{{/unless}} {{/each}}\n<a href=\"{{RESULT_LINK}}\">View check result</a>\n`
+    this.url = `https://api.telegram.org/bot${props.apiToken}/sendMessage?chat_id=${props.chatId}&text=${this.template}`
+    this.method = 'POST'
+    Session.registerConstruct(this)
+  }
 
-    synthesize() {
-        return {
-            ...super.synthesize(),
-            type: 'WEBHOOK',
-            config: {
-                name: this.name,
-                chat_id: this.chat_id,
-                api_token: this.api_token,
-                url: this.url,
-                template: this.template,
-                method: this.method,
-            },
-        }
+  synthesize () {
+    return {
+      ...super.synthesize(),
+      type: 'WEBHOOK_TELEGRAM',
+      config: {
+        name: this.name,
+        chatId: this.chatId,
+        apiToken: this.apiToken,
+        url: this.url,
+        method: this.method,
+      },
     }
+  }
 }
-
-
