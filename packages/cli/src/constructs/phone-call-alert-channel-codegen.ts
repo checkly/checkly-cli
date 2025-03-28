733e9be3cb03d8d967816f5456ae3beaf4ae259c
Simo Kinnunen <simo@checklyhq.com>
2025-03-28 21:51:25 +0900

    feat: export all generated variables for easier usage from other files

--- packages/cli/src/constructs/phone-call-alert-channel-codegen.ts
+++ packages/cli/src/constructs/phone-call-alert-channel-codegen.ts
@@ -39,6 +39,8 @@ export class PhoneCallAlertChannelCodegen extends Codegen<PhoneCallAlertChannelR
           })
         })
       }))
+
+      builder.export()
     }))
   }
 }
--- packages/cli/src/constructs/slack-alert-channel-codegen.ts
+++ packages/cli/src/constructs/slack-alert-channel-codegen.ts
@@ -39,6 +39,8 @@ export class SlackAlertChannelCodegen extends Codegen<SlackAlertChannelResource>
           })
         })
       }))
+
+      builder.export()
     }))
   }
 }
