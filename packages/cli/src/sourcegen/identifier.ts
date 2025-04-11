0814327ce15e2e639c4ba251121328ba5eb79089
Simo Kinnunen <simo@checklyhq.com>
2025-04-11 16:46:44 +0900

    feat: make string casing a separate utility as it may be used for filenames too

--- packages/cli/src/sourcegen/identifier.ts
+++ packages/cli/src/sourcegen/identifier.ts
@@ -1,5 +1,6 @@
 import { Output } from './output'
 import { Value } from './value'
+import { cased, CaseFormat } from './case'
 
 export class IdentifierValue extends Value {
   value: string
@@ -15,87 +16,14 @@ export class IdentifierValue extends Value {
 }
 
 export interface IdentOptions {
-  format?: 'camelCase' | 'PascalCase' | 'SCREAMING_SNAKE_CASE'
+  format?: CaseFormat
 }
 
 export function ident (value: string, options?: IdentOptions): IdentifierValue {
   const format = options?.format
-  if (format === 'camelCase') {
-    value = camelCase(value)
-  } else if (format === 'SCREAMING_SNAKE_CASE') {
-    value = screamingSnakeCase(value)
+  if (format !== undefined) {
+    value = cased(value, format)
   }
 
   return new IdentifierValue(value)
-}
-
-function wordify (value: string): string[] {
-  const words = []
-
-  let word = ''
-  let small = false
-
-  for (const char of value.split('')) {
-    if (char >= 'a' && char <= 'z') {
-      word += char
-      small = true
-      continue
-    }
-
-    if (word.length !== 0 && char >= '0' && char <= '9') {
-      word += char
-      small = true
-      continue
-    }
-
-    if (char >= 'A' && char <= 'Z') {
-      if (small) {
-        words.push(word)
-        word = ''
-        small = false
-        continue
-      }
-
-      word += char
-      continue
-    }
-
-    if (word.length !== 0) {
-      words.push(word)
-      word = ''
-      small = false
-      continue
-    }
-  }
-
-  if (word.length !== 0) {
-    words.push(word)
-  }
-
-  return words
-}
-
-export function camelCase (value: string): string {
-  return wordify(value).map((word, index) => {
-    if (index === 0) {
-      return word.toLowerCase()
-    }
-    const initial = word.slice(0, 1).toUpperCase()
-    const rest = word.slice(1).toLowerCase()
-    return initial + rest
-  }).join('')
-}
-
-export function pascalCase (value: string): string {
-  return wordify(value).map(word => {
-    const initial = word.slice(0, 1).toUpperCase()
-    const rest = word.slice(1).toLowerCase()
-    return initial + rest
-  }).join('')
-}
-
-export function screamingSnakeCase (value: string): string {
-  return wordify(value).map(word => {
-    return word.toUpperCase()
-  }).join('_')
 }
