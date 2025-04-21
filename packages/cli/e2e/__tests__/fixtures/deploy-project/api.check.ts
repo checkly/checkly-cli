b13f8f95eae7ba340ef808da9c9fbfd6af36f9cd
Simo Kinnunen <simo@checklyhq.com>
2025-04-21 20:21:51 +0900

    feat: include triggerIncident in e2e tests

--- packages/cli/e2e/__tests__/fixtures/deploy-project/api.check.ts
+++ packages/cli/e2e/__tests__/fixtures/deploy-project/api.check.ts
@@ -1,6 +1,8 @@
 /* eslint-disable */
 import { ApiCheck, Frequency } from 'checkly/constructs'
 import { privateLocation } from './private-location.check'
+import { fooService } from './status-page.check'
+
 new ApiCheck('api-check', {
   name: 'Api Check',
   activated: false,
@@ -35,3 +37,24 @@ new ApiCheck('api-check-high-freq', {
   maxResponseTime: 30000
 })
 
+new ApiCheck('api-check-incident-trigger', {
+  name: 'Api Check with Incident Trigger',
+  activated: false,
+  frequency: Frequency.EVERY_30S,
+  request: {
+    url: 'https://www.google.com',
+    method: 'GET',
+    followRedirects: false,
+    skipSSL: false,
+    assertions: []
+  },
+  degradedResponseTime: 20000,
+  maxResponseTime: 30000,
+  triggerIncident: {
+    service: fooService,
+    severity: 'MEDIUM',
+    name: 'Connectivity disrupted',
+    description: 'We have detected a disruption in connectivity.',
+    notifySubscribers: true,
+  }
+})
