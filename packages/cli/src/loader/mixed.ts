5b5641482f2d941cacb2f288816ec5bd940cb3b2
Simo Kinnunen <simo@checklyhq.com>
2025-05-19 18:46:39 +0900

    feat: more intelligent file loader [sc-24135] (#1066)
    * feat: more intelligent file loader
    
    Files are now loaded with a more intelligent loader that's able to utilize
    several different options. Additionally it is able to simply utilize the
    native loader in runtimes that support TypeScript natively.
    
    * fix: add err stack back to ts-node load failure
    
    It was there before, keep it as is.

---
+++ packages/cli/src/loader/mixed.ts
@@ -0,0 +1,45 @@
+import {
+  FileLoader,
+  UnsupportedFileLoaderError,
+} from './loader'
+
+export class MixedFileLoader extends FileLoader {
+  loaders: Set<FileLoader>
+
+  constructor (...loaders: FileLoader[]) {
+    super()
+    this.loaders = new Set(loaders)
+  }
+
+  isAuthoritativeFor (filePath: string): boolean {
+    for (const loader of this.loaders) {
+      if (loader.isAuthoritativeFor(filePath)) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  async loadFile<T = unknown> (filePath: string): Promise<T> {
+    for (const loader of this.loaders) {
+      if (loader.isAuthoritativeFor(filePath)) {
+        try {
+          return loader.loadFile<T>(filePath)
+        } catch (err) {
+          if (err instanceof UnsupportedFileLoaderError) {
+            // We'll always get the same error. Just remove the loader to
+            // avoid calling it again.
+            this.loaders.delete(loader)
+          }
+
+          throw err
+        }
+      }
+    }
+
+    throw new UnsupportedFileLoaderError(
+      `Unable to find authoritative loader for file '${filePath}'`,
+    )
+  }
+}
--- packages/cli/src/services/util.ts
+++ packages/cli/src/services/util.ts
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 import type { CreateAxiosDefaults } from 'axios'
 import * as path from 'path'
 import * as fs from 'fs/promises'
 import * as fsSync from 'fs'
-import { Service } from 'ts-node'
 import gitRepoInfo from 'git-repo-info'
 import { parse } from 'dotenv'
 // @ts-ignore
@@ -50,105 +48,6 @@ export function findFilesRecursively (directory: string, ignoredPaths: Array<str
     }
   }
   return files
-}
-
-export async function loadFile (filepath: string): Promise<any> {
-  try {
-    let exported: any
-    if (/\.[mc]?ts$/.test(filepath)) {
-      exported = await loadTsFileDefault(filepath)
-    } else {
-      const imported = await import(pathToPosix(filepath))
-      exported = imported.default ?? {}
-    }
-    if (typeof exported === 'function') {
-      exported = await exported()
-    }
-    return exported
-  } catch (err: any) {
-    throw new Error(`Error loading file ${filepath}\n${err.stack}`)
-  }
-}
-
-async function loadTsFileDefault (filepath: string): Promise<any> {
-  const jiti = await getJiti()
-  if (jiti) {
-    return jiti.import<any>(filepath, {
-      default: true,
-    })
-  }
-
-  // Backward-compatibility for users who installed ts-node.
-  const tsCompiler = await getTsNodeService()
-  if (tsCompiler) {
-    tsCompiler.enabled(true)
-    let exported: any
-    try {
-      exported = (await require(filepath)).default
-    } catch (err: any) {
-      if (err.message && err.message.includes('Unable to compile TypeScript')) {
-        throw new Error(`Consider installing "jiti" instead of "ts-node" for improved TypeScript support\n${err.stack}`)
-      }
-      throw err
-    } finally {
-      tsCompiler.enabled(false) // Re-disable the TS compiler
-    }
-    return exported
-  }
-
-  throw new Error('Please install "jiti" to use TypeScript files')
-}
-
-// Regular type import gave issue with jest.
-type Jiti = ReturnType<(typeof import('jiti', {
-  with: {
-    'resolution-mode': 'import'
-  }
-}))['createJiti']>
-
-// To avoid a dependency on typescript for users with no TS checks, we need to dynamically import jiti
-let jiti: Jiti
-let haveJiti = false
-async function getJiti (): Promise<Jiti | undefined> {
-  if (haveJiti) return jiti
-  try {
-    const maybeJiti = await import('jiti')
-    // Jiti 1x does not have createJiti().
-    if (typeof maybeJiti.createJiti !== 'function') {
-      return
-    }
-    jiti = maybeJiti.createJiti(__filename)
-    haveJiti = true
-  } catch (err: any) {
-    if (err.code === 'ERR_MODULE_NOT_FOUND' || err.code === 'MODULE_NOT_FOUND') {
-      return undefined
-    }
-    throw err
-  }
-  return jiti
-}
-
-// To avoid a dependency on typescript for users with no TS checks, we need to dynamically import ts-node
-let tsNodeService: Service
-async function getTsNodeService (): Promise<Service | undefined> {
-  if (tsNodeService) return tsNodeService
-  try {
-    const tsNode = await import('ts-node')
-    tsNodeService = tsNode.register({
-      moduleTypes: {
-        '**/*': 'cjs',
-      },
-      compilerOptions: {
-        module: 'CommonJS',
-      },
-    })
-  } catch (err: any) {
-    if (err.code === 'ERR_MODULE_NOT_FOUND' || err.code === 'MODULE_NOT_FOUND') {
-      return undefined
-    }
-    throw err
-  }
-  return tsNodeService
 }
 
 /**
