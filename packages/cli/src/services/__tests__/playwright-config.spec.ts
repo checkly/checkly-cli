26e4ea82952f247b1a865660c8bb318cfece27d2
Ferran Diaz <ferran@checklyhq.com>
2025-05-22 11:42:52 +0200

    fix: tests

--- packages/cli/src/services/__tests__/playwright-config.spec.ts
+++ packages/cli/src/services/__tests__/playwright-config.spec.ts
@@ -9,13 +9,13 @@ const fixturesPath = path.join(__dirname, 'fixtures', 'playwright-configs')
 describe('playwright-config', () => {
   it('it should load simple config correctly', async () => {
     const pwConfig = await Session.loadFile(path.join(fixturesPath, 'simple-config.ts'))
-    const config = new PlaywrightConfig(fixturesPath, pwConfig, 'simple-config.ts')
+    const config = new PlaywrightConfig(path.join(fixturesPath, 'simple-config.ts'), pwConfig)
     expect(Array.from(config.testMatch)).toEqual(['**/*.@(spec|test).?(c|m)[jt]s?(x)'])
     expect(config.getBrowsers()).toEqual(['chromium', 'webkit', 'msedge', 'chrome'])
   })
   it('it should load simple config correctly', async () => {
     const pwConfig = await Session.loadFile(path.join(fixturesPath,'simple-config-no-browsers.ts'))
-    const config = new PlaywrightConfig(fixturesPath, pwConfig, 'simple-config-no-browsers.ts')
+    const config = new PlaywrightConfig(path.join(fixturesPath,'simple-config-no-browsers.ts'), pwConfig)
     expect(Array.from(config.testMatch)).toEqual(['tests.*.ts'])
     expect(config.getBrowsers()).toEqual(['chromium'])
   })
