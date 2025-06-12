117bfc49ca57048e7bac2e36e3a19c2e54829d63
Ferran Diaz <ferran@checklyhq.com>
2025-06-12 12:19:05 +0200

    feat: introduce pwTags and pwProjects

--- packages/cli/src/constructs/playwright-check.ts
+++ packages/cli/src/constructs/playwright-check.ts
@@ -73,6 +73,10 @@ export class PlaywrightCheck extends Check {
     }
   }
 
+  allowInChecklyConfig () {
+    return true
+  }
+
   #findGroupByName (groupName: string) {
     return Object.values(Session.project?.data?.['check-group'] ?? {})
       .find(group => group.name === groupName)
