3e3d34ee37cf6d7b1b7f2705a494db2462df5f16
Simo Kinnunen <simo@checklyhq.com>
2025-06-16 15:22:05 +0900

    feat: introduce CheckGroupV2 which avoids unintuitive implicit defaults [sc-24260] (#1080)
    * feat: document behavior of group properties
    
    * feat: introduce CheckGroupV2 which avoids unintuitive implicit defaults
    
    The current CheckGroup continues to work the same as before, however a
    warning is output recommending the user to upgrade to CheckGroupV2.
    
    * fix: update doc comments for activated/muted
    
    * feat: make activated and muted optional again, final decision
    
    The default values are good enough to not need to consider a potential future
    `null` value for these properties.
    
    * feat: only generate activated/muted props for CheckGroupV2 if not defaults
    
    * feat: handle useGlobalAlertSettings's new behavior
    
    * chore: add a link to the update guide
    
    * feat: produce validation error if both doubleCheck and retryStrategy are set
    
    * chore: fix typo
    
    * fix: use proper props type for CheckGroupV1
    
    * fix(tests): test output uses constructor name
    
    * feat: use CheckGroupV2 in e2e tests
    
    * feat: remove support for doubleCheck in CheckGroupV2
    
    The property is still defined but deprecated, and if used, will error upon
    validation.

---
+++ packages/cli/src/constructs/check-group-ref.ts
@@ -0,0 +1,38 @@
+import { CheckConfigDefaults } from '../services/checkly-config-loader'
+import { CheckGroupV1 } from './check-group-v1'
+import { Construct } from './construct'
+import { Session } from './project'
+
+/**
+ * Creates a reference to an existing Check Group (both v1 and v2).
+ *
+ * References link existing resources to a project without managing them.
+ */
+export class CheckGroupRef extends Construct {
+  constructor (logicalId: string, physicalId: string|number) {
+    super(CheckGroupV1.__checklyType, logicalId, physicalId, false)
+    Session.registerConstruct(this)
+  }
+
+  public getCheckDefaults (): CheckConfigDefaults {
+    // The only value CheckGroup.getCheckDefaults() returns is frequency,
+    // which exists purely for convenience, and only at CLI-level. It never
+    // gets sent to the backend. If references are being used, no access to
+    // this feature is required, and it can be ignored entirely.
+    return {}
+  }
+
+  public getBrowserCheckDefaults (): CheckConfigDefaults {
+    // See the comment for getCheckDefaults(), the same applies here.
+    return {}
+  }
+
+  public getMultiStepCheckDefaults (): CheckConfigDefaults {
+    // See the comment for getCheckDefaults(), the same applies here.
+    return {}
+  }
+
+  synthesize () {
+    return null
+  }
+}
---
+++ packages/cli/src/constructs/check-group-v2.ts
@@ -0,0 +1,192 @@
+import { PrivateLocation, PrivateLocationRef } from './private-location'
+import type { Region } from '..'
+import {
+  type RetryStrategy,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  type RetryStrategyBuilder, // Used for @links in comments.
+} from './retry-strategy'
+import {
+  AlertEscalation,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  type AlertEscalationBuilder, // Used for @links in comments.
+} from './alert-escalation-policy'
+import { Diagnostics } from './diagnostics'
+import { CheckGroupV1, CheckGroupV1Props } from './check-group-v1'
+import { RemovedPropertyDiagnostic } from './construct-diagnostics'
+
+export interface CheckGroupV2Props extends Omit<CheckGroupV1Props, 'alertEscalationPolicy'> {
+  /**
+   * This property is no longer supported; use {@link retryStrategy} instead.
+   *
+   * To match the behavior of `doubleCheck: true`, use:
+   *
+   *     retryStrategy: RetryStrategyBuilder.fixedStrategy({
+   *       maxRetries: 1,
+   *       baseBackoffSeconds: 0,
+   *       maxDurationSeconds: 600,
+   *       sameRegion: false,
+   *     })
+   *
+   * To match the behavior of `doubleCheck: false`, use:
+   *
+   *     retryStrategy: RetryStrategyBuilder.noRetries()
+   *
+   * @deprecated Use {@link CheckGroupV2Props.retryStrategy} instead.
+   */
+  doubleCheck?: boolean
+
+  /**
+   * An array of one or more data center locations where to run the checks.
+   *
+   * If either {@link CheckGroupV2Props.locations} or
+   * {@link CheckGroupV2Props.privateLocations} is set to a non-empty value, all
+   * checks in the group will use those values instead of their own.
+   */
+  locations?: (keyof Region)[]
+
+  /**
+   * An array of one or more private locations where to run the checks.
+   *
+   * If either {@link CheckGroupV2Props.locations} or
+   * {@link CheckGroupV2Props.privateLocations} is set to a non-empty value, all
+   * checks in the group will use those values instead of their own.
+   */
+  privateLocations?: (string | PrivateLocation | PrivateLocationRef)[]
+
+  /**
+   * When {@link AlertEscalation}, all checks in the group will use the
+   * group's alert escalation policy. Use {@link AlertEscalationBuilder} to
+   * build a suitable policy.
+   *
+   * When `"global"`, all checks in the group will use the global alert
+   * escalation policy.
+   *
+   * If not set, individual check settings are used.
+   */
+  alertEscalationPolicy?: AlertEscalation | 'global',
+
+  /**
+   * Sets a retry policy for the group. Use {@link RetryStrategyBuilder} to
+   * create a retry policy.
+   *
+   * If set, all checks in the group use the group's retry strategy.
+   *
+   * If not set, individual check settings are used.
+   */
+  retryStrategy?: RetryStrategy
+
+  /**
+   * Determines whether the checks in the group should run on all selected
+   * locations in parallel or round-robin.
+   *
+   * When `true`, all checks in the group run in parallel regardless of their
+   * individual setting.
+   *
+   * When `false`, all checks in the group run in round-robin regardless of
+   * their individual setting.
+   *
+   * If not set, individual check settings are used.
+   *
+   * See https://www.checklyhq.com/docs/monitoring/global-locations/ to learn
+   * more about scheduling strategies.
+   */
+  runParallel?: boolean
+}
+
+/**
+ * Creates a Check Group (v2).
+ *
+ * The following properties have changed since CheckGroupV1:
+ *
+ *   - {@link CheckGroupV2Props.alertEscalationPolicy}
+ *     - The implicit default for this property has been removed, allowing
+ *       individual check settings to take effect.
+ *     - Can be set to `"global"` to match the earlier default behavior.
+ *   - {@link CheckGroupV2Props.retryStrategy}
+ *     - The implicit default for this property has been removed, allowing
+ *       individual check settings to take effect.
+ *   - {@link CheckGroupV2Props.runParallel}
+ *     - The implicit default for this property has been removed, allowing
+ *       individual check settings to take effect.
+ */
+export class CheckGroupV2 extends CheckGroupV1 {
+  constructor (logicalId: string, props: CheckGroupV2Props) {
+    const { alertEscalationPolicy, useGlobalAlertSettings } = (() => {
+      const { alertEscalationPolicy } = props
+
+      // Do we want to always use the global policy?
+      if (alertEscalationPolicy === 'global') {
+        return {
+          alertEscalationPolicy: undefined,
+          useGlobalAlertSettings: true,
+        }
+      }
+
+      // Do we want to let checks keep their own policies?
+      if (alertEscalationPolicy === undefined) {
+        return {
+          alertEscalationPolicy,
+          useGlobalAlertSettings: undefined,
+        }
+      }
+
+      // The group policy will always apply.
+      return {
+        alertEscalationPolicy,
+        useGlobalAlertSettings: false,
+      }
+    })()
+
+    super(logicalId, {
+      ...props,
+      alertEscalationPolicy,
+    })
+
+    // Must override; the V1 constructor will not give us the desired behavior.
+    this.useGlobalAlertSettings = useGlobalAlertSettings
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async onBeforeValidate (diagnostics: Diagnostics): Promise<void> {
+    // No-op
+  }
+
+  protected async validateDoubleCheck (diagnostics: Diagnostics): Promise<void> {
+    if (this.doubleCheck !== undefined) {
+      if (this.doubleCheck) {
+        diagnostics.add(new RemovedPropertyDiagnostic(
+          'doubleCheck',
+          new Error(
+            `To match the behavior of doubleCheck: true, please use the ` +
+            `following retryStrategy instead:` +
+            `\n\n` +
+            `  RetryStrategyBuilder.fixedStrategy({\n` +
+            `    maxRetries: 1,\n` +
+            `    baseBackoffSeconds: 0,\n`+
+            `    maxDurationSeconds: 600,\n` +
+            `    sameRegion: false,\n` +
+            `  })`,
+          ),
+        ))
+      } else {
+        diagnostics.add(new RemovedPropertyDiagnostic(
+          'doubleCheck',
+          new Error(
+            `To match the behavior of doubleCheck: false, please use the ` +
+            `following retryStrategy instead:` +
+            `\n\n` +
+            `  RetryStrategyBuilder.noRetries()`,
+          ),
+        ))
+      }
+    }
+  }
+
+  synthesize() {
+    return {
+      ...super.synthesize(),
+      doubleCheck: undefined,
+      v: 2,
+    }
+  }
+}
