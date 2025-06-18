f2ebb1d13c98069d08291f056461b6f016c8145b
Ferran Diaz <ferran@checklyhq.com>
2025-06-18 11:15:30 +0200

    feat: create base pw-test command

---
+++ packages/cli/src/commands/pw-test.ts
@@ -0,0 +1,211 @@
+import { AuthCommand } from './authCommand'
+import { getCiInformation, getGitInformation, splitConfigFilePath } from '../services/util'
+import { loadChecklyConfig, PlaywrightSlimmedProp } from '../services/checkly-config-loader'
+import { prepareReportersTypes, prepareRunLocation } from '../helpers/test-helper'
+import * as api from '../rest/api'
+import config from '../services/config'
+import { parseProject } from '../services/project-parser'
+import type { Runtime } from '../rest/runtimes'
+import { Diagnostics, Session } from '../constructs'
+import { ux } from '@oclif/core'
+import { createReporters } from '../reporters/reporter'
+import TestRunner from '../services/test-runner'
+import { Events, SequenceId } from '../services/abstract-check-runner'
+import { TestResultsShortLinks } from '../rest/test-sessions'
+const DEFAULT_REGION = 'eu-central-1'
+
+
+export default class PwTestCommand extends AuthCommand {
+  static coreCommand = true
+  static hidden = false
+  static description = 'Test your Playwright Tests on Checkly'
+  static state = 'beta'
+
+  async run(): Promise<void> {
+    this.style.actionStart('Parsing your Playwright project')
+    const rawArgs = this.argv || []
+
+    const { configDirectory, configFilenames } = splitConfigFilePath()
+    const {
+      config: checklyConfig,
+      constructs: checklyConfigConstructs,
+    } = await loadChecklyConfig(configDirectory, configFilenames)
+
+    // TODO: ADD PROPER LOCATION HANDLING
+    const location = await prepareRunLocation(checklyConfig.cli, {}, api, config.getAccountId())
+
+    // TODO: SET PROPER REPORTER TYPES
+    const reporterTypes = prepareReportersTypes('list', checklyConfig.cli?.reporters)
+    const { data: account } = await api.accounts.get(config.getAccountId())
+    const { data: availableRuntimes } = await api.runtimes.getAll()
+
+    const project = await parseProject({
+      directory: configDirectory,
+      projectLogicalId: checklyConfig.logicalId,
+      // TODO: ADD PROPPER TEST SESSION NAME HANDLING
+      projectName: checklyConfig.projectName,
+      repoUrl: checklyConfig.repoUrl,
+      includeTestOnlyChecks: true,
+      checkMatch: checklyConfig.checks?.checkMatch,
+      ignoreDirectoriesMatch: checklyConfig.checks?.ignoreDirectoriesMatch,
+      checkDefaults: checklyConfig.checks,
+      availableRuntimes: availableRuntimes.reduce((acc, runtime) => {
+        acc[runtime.name] = runtime
+        return acc
+      }, <Record<string, Runtime>> {}),
+      defaultRuntimeId: account.runtimeId,
+      verifyRuntimeDependencies: false,
+      checklyConfigConstructs,
+      playwrightConfigPath: checklyConfig.checks?.playwrightConfigPath,
+      include: checklyConfig.checks?.include,
+      playwrightChecks: PwTestCommand.createPlaywrightCheck(rawArgs),
+      checkFilter: check => {
+        return true
+      }
+    })
+
+    this.style.actionSuccess()
+
+    this.style.actionStart('Validating project resources')
+
+    const diagnostics = new Diagnostics()
+    await project.validate(diagnostics)
+
+    for (const diag of diagnostics.observations) {
+      if (diag.isFatal()) {
+        this.style.longError(diag.title, diag.message)
+      } else if (!diag.isBenign()) {
+        this.style.longWarning(diag.title, diag.message)
+      } else {
+        this.style.longInfo(diag.title, diag.message)
+      }
+    }
+
+    if (diagnostics.isFatal()) {
+      this.style.actionFailure()
+      this.style.shortError(`Unable to continue due to unresolved validation errors.`)
+      this.exit(1)
+    }
+
+    this.style.actionSuccess()
+
+    this.style.actionStart('Bundling project resources')
+    const projectBundle = await (async () => {
+      try {
+        const bundle = await project.bundle()
+        this.style.actionSuccess()
+        return bundle
+      } catch (err) {
+        this.style.actionFailure()
+        throw err
+      }
+    })()
+
+    const checkBundles = Object.values(projectBundle.data.check)
+
+    if (this.fancy) {
+      ux.action.stop()
+    }
+
+    if (!checkBundles.length) {
+      this.log(`Unable to find checks to run`)
+      return
+    }
+
+    // TODO: ADD PROPER LIST FLAG HANDLING
+    // if (list) {
+    //   this.listChecks(checkBundles.map(({ construct }) => construct))
+    //   return
+    // }
+
+    // TODO: ADD PROPER VERBOSE FLAG HANDLING
+    const reporters = createReporters(reporterTypes, location, false)
+    const repoInfo = getGitInformation(project.repoUrl)
+    const ciInfo = getCiInformation()
+    // TODO: ADD PROPER RETRY STRATEGY HANDLING
+    // const testRetryStrategy = this.prepareTestRetryStrategy(retries, checklyConfig?.cli?.retries)
+
+    const runner = new TestRunner(
+      config.getAccountId(),
+      projectBundle,
+      checkBundles,
+      Session.sharedFiles,
+      location,
+      // TODO: ADD PROPER TEST SESSION TIMEOUT HANDLING
+      1000,
+      // TODO: ADD PROPER VERBOSE FLAG HANDLING
+      false,
+      // TODO: ADD PROPER RECORD FLAG HANDLING
+      true,
+      repoInfo,
+      ciInfo.environment,
+      // NO NEED TO UPLOAD SNAPSHOTS FOR PLAYWRIGHT TESTS
+      false,
+      configDirectory,
+      // TODO: ADD PROPER RETRY STRATEGY HANDLING
+      null, // testRetryStrategy
+    )
+
+    runner.on(Events.RUN_STARTED,
+      (checks: Array<{ check: any, sequenceId: SequenceId }>, testSessionId: string) =>
+        reporters.forEach(r => r.onBegin(checks, testSessionId)),
+    )
+
+    runner.on(Events.CHECK_INPROGRESS, (check: any, sequenceId: SequenceId) => {
+      reporters.forEach(r => r.onCheckInProgress(check, sequenceId))
+    })
+
+    runner.on(Events.MAX_SCHEDULING_DELAY_EXCEEDED, () => {
+      reporters.forEach(r => r.onSchedulingDelayExceeded())
+    })
+
+    runner.on(Events.CHECK_ATTEMPT_RESULT, (sequenceId: SequenceId, check, result, links?: TestResultsShortLinks) => {
+      reporters.forEach(r => r.onCheckAttemptResult(sequenceId, {
+        logicalId: check.logicalId,
+        sourceFile: check.getSourceFile(),
+        ...result,
+      }, links))
+    })
+
+    runner.on(Events.CHECK_SUCCESSFUL,
+      (sequenceId: SequenceId, check, result, testResultId, links?: TestResultsShortLinks) => {
+        if (result.hasFailures) {
+          process.exitCode = 1
+        }
+
+        reporters.forEach(r => r.onCheckEnd(sequenceId, {
+          logicalId: check.logicalId,
+          sourceFile: check.getSourceFile(),
+          ...result,
+        }, testResultId, links))
+      })
+
+    runner.on(Events.CHECK_FAILED, (sequenceId: SequenceId, check, message: string) => {
+      reporters.forEach(r => r.onCheckEnd(sequenceId, {
+        ...check,
+        logicalId: check.logicalId,
+        sourceFile: check.getSourceFile(),
+        hasFailures: true,
+        runError: message,
+      }))
+      process.exitCode = 1
+    })
+    runner.on(Events.RUN_FINISHED, () => reporters.forEach(r => r.onEnd()))
+    runner.on(Events.ERROR, (err) => {
+      reporters.forEach(r => r.onError(err))
+      process.exitCode = 1
+    })
+    await runner.run()
+
+
+    }
+    static createPlaywrightCheck(args: string[]): PlaywrightSlimmedProp [] {
+      const input = args.join(' ') || ''
+     return [{
+        logicalId: 'playwright-check',
+        name: `Playwright Test: ${input}`,
+        testCommand: `npx playwright test ${input}`,
+        locations: [DEFAULT_REGION],
+      }]
+  }
+}
