30e8758921ef2eb08db22ab7bb7cb783ac51aec1
Ferran Diaz <ferran@checklyhq.com>
2025-06-20 10:33:01 +0200

    feat: add tests

---
+++ packages/cli/e2e/__tests__/fixtures/test-pwt-native/test-project/pw-test-project.test.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import { test, expect } from '@playwright/test'
+
+
+test('Tag B test project @TAG-B', async ({ page }) => {
+    expect(true).toBe(true);
+});
---
+++ packages/cli/e2e/__tests__/fixtures/test-pwt-native/test/pw-test-example.test.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+import { test, expect } from '@playwright/test'
+
+test('Tag A test example @TAG-A', async ({ page }) => {
+    expect(true).toBe(true);
+});
+
+test('Tag B test example @TAG-B', async ({ page }) => {
+    expect(true).toBe(true);
+});
