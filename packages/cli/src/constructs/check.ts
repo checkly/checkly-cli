dfa33fba668d1670dc81aab9d130a1457df0ab29
Hannes Lenke <hannes@lenke.at>
2025-07-08 04:42:01 -0400

    Update check.ts. Delete extensive list of locations.

--- packages/cli/src/constructs/check.ts
+++ packages/cli/src/constructs/check.ts
@@ -73,11 +73,6 @@ export interface CheckProps {
   
   /**
    * An array of one or more data center locations where to run this check.
-   * The supported regions are:
-   * us-east-1, us-east-2, us-west-1, us-west-2, ca-central-1, sa-east-1,
-   * eu-west-1, eu-central-1, eu-west-2, eu-west-3, eu-north-1, eu-south-1, me-south-1,
-   * ap-southeast-1, ap-northeast-1, ap-east-1, ap-southeast-2, ap-southeast-3, ap-northeast-2, ap-northeast-3,
-   * ap-south-1, af-south-1
    * 
    * @example ['us-east-1', 'eu-west-1', 'ap-southeast-1']
    * @see {@link https://www.checklyhq.com/docs/monitoring/global-locations/ | Global Locations}
