70d9cde7a4908e8caea6ea914ef96c72a10b7a6f
The Typescriptrix <noctnica@gmail.com>
2025-08-22 07:30:37 -0700

    Update examples/advanced-project/src/__checks__/utils/website-groups.check.ts
    Co-authored-by: Stefan Judis <stefanjudis@gmail.com>

--- examples/advanced-project/src/__checks__/utils/website-groups.check.ts
+++ examples/advanced-project/src/__checks__/utils/website-groups.check.ts
@@ -11,7 +11,7 @@ export const syntheticGroup = new CheckGroupV2('check-group-synthetics', {
   frequency: Frequency.EVERY_15M,
   locations: ['us-east-1', 'eu-west-1'],
   tags: ['synthetics'],
-  // by setting an alertEscalationPolicy, these settings will override those on individual checks
+  // By setting an alertEscalationPolicy, these settings will override those on individual checks
   alertEscalationPolicy: AlertEscalationBuilder.runBasedEscalation(
     2, // Alert after 2 consecutive failures
     { interval: 5, amount: 2 }, // Send 2 reminders, 5 minutes apart
