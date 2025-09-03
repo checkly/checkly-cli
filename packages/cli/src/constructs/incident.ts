3ef604b65dc7f450be39af0043764aee8a868af3
Stefan Judis <stefanjudis@gmail.com>
2025-09-03 23:01:36 +0200

    docs: add simple TSDocs for `IncidentTrigger` (#1141)

--- packages/cli/src/constructs/incident.ts
+++ packages/cli/src/constructs/incident.ts
@@ -3,9 +3,14 @@ import { StatusPageService } from './status-page-service'
 type IncidentSeverity = 'MINOR' | 'MEDIUM' | 'MAJOR' | 'CRITICAL'
 
 export interface IncidentTrigger {
+  /** The status page service that this incident will be associated with. */
   service: StatusPageService
+  /** The severity level of the incident. */
   severity: IncidentSeverity
+  /** The name of the incident. */
   name: string
+  /** A detailed description of the incident. */
   description: string
+  /** Whether to notify subscribers when the incident is triggered. */
   notifySubscribers: boolean
 }
