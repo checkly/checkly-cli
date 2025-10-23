f66485af82ce3b8b75157f73980046fd7b8ba6d3
miliberlin <michelle.liebheit@posteo.de>
2025-10-23 11:18:15 +0200

    feat: export function correctly in common js [tra-1519]

--- examples/advanced-project-js/src/__checks__/utils/auth-client.js
+++ examples/advanced-project-js/src/__checks__/utils/auth-client.js
@@ -1,8 +1,10 @@
 // This auth client is a stub of the script you'd want to run to retrieve an
 // auth token. Read more about authentication in API monitors here: https://www.checklyhq.com/docs/detect/synthetic-monitoring/api-checks/examples/#user-authentication-apis
 
-export async function getToken() {
+async function getToken() {
   console.log('Fetching a token from an imaginary auth API endpoint')
   const token = await new Promise((resolve) => resolve('123abc'))
   return token
 }
+
+module.exports = { getToken }
