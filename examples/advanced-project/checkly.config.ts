da96026d2aab7e11a17b1f17a50b3c2d18d1fa2a
Eddie Jaoude <eddie@jaoudestudios.com>
2025-12-15 08:10:39 +0000

    fix: typo in example config (#1187)
    After using the cli to create a new project and selecting the advanced template, when running the test command the following error occurs:
    
     npx checkly test --record
    Parsing your project... !
        Error: Error loading file '/Users/eddie/Downloads/repos/checkly/checkly.config.ts'
        Error: ParseError: Unexpected token  
         /Users/eddie/Downloads/repos/checkly/checkly.config.ts:29:52

--- examples/advanced-project/checkly.config.ts
+++ examples/advanced-project/checkly.config.ts
@@ -26,7 +26,7 @@ const config = defineConfig({
      */
     runtimeId: '2025.04',
     /* Failed check runs will not be retried per default. See `browserChecks` below for a retry example. */
-    retryStrategy: RetryStrategyBuilder.noRetries(),,
+    retryStrategy: RetryStrategyBuilder.noRetries(),
     /* All checks will have this alert escalation policy defined */
     alertEscalationPolicy: AlertEscalationBuilder.runBasedEscalation(1),
     /* A glob pattern that matches the Checks inside your repo, see https://www.checklyhq.com/docs/constructs/including-checks/#checks-checkmatch */
