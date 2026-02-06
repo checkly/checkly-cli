e55b1c5193663b7193a1e451f22a91a91e1601e7
Pilar Martinez <pilar@checklyhq.com>
2026-02-06 11:58:12 -0300

    fix: test [red-120]

--- packages/cli/src/helpers/__tests__/recast-acorn-parser.spec.ts
+++ packages/cli/src/helpers/__tests__/recast-acorn-parser.spec.ts
@@ -3,12 +3,14 @@ import * as recast from 'recast'
 import * as acornParser from '../recast-acorn-parser'
 import { findPropertyByName } from '../write-config-helpers'
 
+const normalizeLineEndings = (str: string) => str.replace(/\r\n/g, '\n')
+
 describe('recast-acorn-parser', () => {
   it('should parse numeric literals with underscore separators', () => {
     const source = 'const x = 1_000_000'
     const ast = recast.parse(source, { parser: acornParser })
     const output = recast.print(ast).code
-    expect(output).toBe(source)
+    expect(normalizeLineEndings(output)).toBe(source)
   })
 
   it('should parse config-like code with underscore-separated numbers', () => {
@@ -19,18 +21,18 @@ describe('recast-acorn-parser', () => {
     const ast = recast.parse(source, { parser: acornParser })
     const prop = findPropertyByName(ast, 'timeout')
     expect(prop).toBeDefined()
-    expect(recast.print(ast).code).toBe(source)
+    expect(normalizeLineEndings(recast.print(ast).code)).toBe(source)
   })
 
   it('should preserve comments', () => {
     const source = '// a comment\nconst x = 1'
     const ast = recast.parse(source, { parser: acornParser })
-    expect(recast.print(ast).code).toBe(source)
+    expect(normalizeLineEndings(recast.print(ast).code)).toBe(source)
   })
 
   it('should parse module syntax', () => {
     const source = 'import foo from \'bar\'\nexport default foo'
     const ast = recast.parse(source, { parser: acornParser })
-    expect(recast.print(ast).code).toBe(source)
+    expect(normalizeLineEndings(recast.print(ast).code)).toBe(source)
   })
 })
